cc5bcf6ef448689f5aea02d71c9bb3e7a152d30a
zx <wwweagle@gmail.com>
2019-10-18 10:04:45 +0800

    WIP

---
+++ parseDPAFR.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 17:24:12 2019
+
+@author: Libra
+"""
+
+import numpy as np
+import pandas as pd
+import h5py
+import math
+import matplotlib.pyplot as plt
+
+def trialAlign(trials,oneTS):
+    oneTS=oneTS[np.bitwise_and(oneTS>=trials[0,0]-30000*5, oneTS<=(trials[-1,0]+30000*10))]
+    TSidx=0
+    tIdx=0;
+    tsInfo=np.ones_like(oneTS);
+    while TSidx<len(oneTS) and tIdx<len(trials):
+        if oneTS[TSidx]<trials[tIdx,0]+(trials[tIdx,7]+8)*30000:
+            oneTS[TSidx]-=trials[tIdx,0]
+            tsInfo[TSidx]=tIdx
+            TSidx+=1
+        else:
+            tIdx+=1
+    return (oneTS,tsInfo)
+        
+def baselineVector(oneTS,tsInfo):
+    tIdices=np.unique(tsInfo)
+    base=[]
+    for tIdx in tIdices:
+        base.append(np.sum(np.bitwise_and(np.bitwise_and(tsInfo==tIdx, oneTS>=-60000), oneTS<0))/4) #500ms bin
+    if len(base)>0 and np.std(base):
+        return (np.mean(base), np.std(base))
+    else:
+        return (0,32767)
+
+def toHist(trials,oneTS,tsInfo,sample,delay,baseVector):
+    sel=np.nonzero(np.bitwise_and(trials[:,4]==sample , trials[:,7]==delay))[0]
+    return ((np.histogram(oneTS[np.isin(tsInfo,sel)],np.linspace(-60000,30000*(delay+6),num=(delay+8)*2+1))[0])/len(sel)-baseVector[0])/baseVector[1]
+
+           
+
+
+def alignHeatmap(spkTS,spkCluster,unitInfo,trials):
+    heat43=[]
+    heat46=[]
+    heat83=[]
+    heat86=[]
+    
+    spkNThresh=spkTS[-1]/s1s*2    
+    
+    for idx in range(unitInfo.shape[0]):
+        wf=unitInfo.iloc[idx,8]=='good' or (math.isnan(unitInfo.iloc[idx,8]) and unitInfo.iloc[idx,3]=='good')
+        spkCount=unitInfo.iloc[idx,9]
+        if spkCount>spkNThresh and wf:
+            oneTSAll=(spkTS[spkCluster==idx]).astype('int64')
+            (oneTS,tsInfo)=trialAlign(trials,oneTSAll)
+            baseVec=baselineVector(oneTS,tsInfo)
+#            sel=np.nonzero(np.bitwise_and(trials[:,4]==4 , trials[:,7]==3))[0]
+            heat43.append(toHist(trials,oneTS,tsInfo,4,3,baseVec))
+            heat46.append(toHist(trials,oneTS,tsInfo,4,6,baseVec))
+            heat83.append(toHist(trials,oneTS,tsInfo,8,3,baseVec))
+            heat86.append(toHist(trials,oneTS,tsInfo,8,6,baseVec))
+    return (heat43,heat46,heat83,heat86)
+
+def plotOne(data,delay,ax,ylbl):
+    
+    plt.imshow(data,cmap='jet',aspect='auto',vmin=-3,vmax=3)
+    
+    
+    if delay==6:
+        [plt.plot([x,x],ax.get_ylim(),'-w') for x in [4.5,6.5,18.5,20.5]]
+        ax.set_xticks([4.5,14.5,24.5])
+        ax.set_xticklabels([0,5,10])
+        ax.set_xlabel('Time (s)')
+        
+    elif delay==3:
+        [plt.plot([x,x],ax.get_ylim(),'-w') for x in [4.5,6.5,12.5,14.5]]
+        ax.set_xticks([4.5,14.5])
+        ax.set_xticklabels([0,5])
+    
+    if ylbl:
+        ax.set_ylabel('Unit #')
+    
+
+def plotHeatmap(heat43,heat46,heat83,heat86):
+#    plt.figure(0,figsize=[15,15])
+#    plt.imshow(np.array(heat43),cmap='jet',vmin=-3,vmax=3)
+#    plt.figure(1,figsize=[15,15])
+#    plt.imshow(np.array(heat83),cmap='jet',vmin=-3,vmax=3)         
+#    plt.figure(2,figsize=[15,15])
+#    plt.imshow(np.array(heat46),cmap='jet',vmin=-3,vmax=3)
+    fh=plt.figure(3,figsize=[7.5,10])
+    ax=plt.subplot(2,2,1)
+    plotOne(np.array(heat43),3,ax,True);
+    ax=plt.subplot(2,2,2)
+    plotOne(np.array(heat83),3,ax,False);
+    ax=plt.subplot(2,2,3)
+    plotOne(np.array(heat46),6,ax,True);
+    ax=plt.subplot(2,2,4)
+    plotOne(np.array(heat86),6,ax,False);
+    plt.show();
+    fh.savefig('heatmap.png',dpi=300,bbox_inches='tight')
+    return (fh,ax)
+
+
+
+if __name__=="__main__":
+    s1s=30000
+    spkTS=np.load("spike_times.npy")
+    spkCluster=np.load("spike_clusters.npy")
+    
+    unitInfo=pd.read_csv('cluster_info.tsv',sep='\t')
+    
+    trials=np.empty([0])
+    with h5py.File('events.hdf5','r') as fe:
+        dset=fe['trials']
+        trials=np.array(dset,dtype='int64')    
+    (heat43,heat46,heat83,heat86)=alignHeatmap(spkTS,spkCluster,unitInfo,trials)
+    (fh,ax)=plotHeatmap(heat43,heat46,heat83,heat86)
---
+++ zxSort.m
@@ -0,0 +1,85 @@
+tic
+addpath('D:\code\neuropixel-utils')
+addpath('D:\code\npy-matlab\npy-matlab\')
+addpath(genpath('D:\code\Kilosort2'))
+channelMapFile='D:\code\neuropixel-utils\map_files\neuropixPhase3B2_kilosortChanMap.mat';
+lwd=pwd();
+fn=ls('*.ap.bin');
+imec=Neuropixel.ImecDataset(fullfile(lwd,fn),'ChannelMap',channelMapFile);
+rmsBadChannels=imec.markBadChannelsByRMS('rmsRange',[3 100]);
+imec.writeModifiedAPMeta();
+fn=replace(fn,'.imec0.','_0.imec.');
+fn=replace(fn,'.imec1.','_1.imec.');
+cleanedPath = fullfile([lwd,'_cleaned'],fn);
+extraMeta = struct();
+extraMeta.commonAverageReferenced = true;
+fnList = {@Neuropixel.DataProcessFn.commonAverageReference};
+imec = imec.saveTranformedDataset(cleanedPath, 'transformAP', fnList, 'extraMeta', extraMeta);
+% imec.inspectAP_timeWindow([1000,1001]);
+Neuropixel.runKiloSort2(imec,'workingDir','D:\temp');
+toc
+%% phy
+
+
+
+%% risky, heavy memory load
+tic
+sync6=imec.readSync();
+% save('sync.mat','sync6');
+syncH5=fullfile([lwd,'_cleaned'],'sync.hdf5');
+h5create(syncH5,'/sync',size(sync6),'Datatype','int8')
+h5write(syncH5,'/sync',int8(sync6))
+
+toc
+
+
+ks=Neuropixel.KiloSortDataset(pwd(),'channelMap',channelMapFile);
+ks.load();
+ks.computeBasicStats();
+metrics=ks.computeMetrics();
+metrics.plotClusterWaveformAtCenterOfMass();
+savefig('centerOfMass.fig');
+TS=ks.spike_times;
+ids=ks.spike_clusters;
+clusterI = readtable('cluster_info.tsv','FileType','text');
+waveformGood=strcmp(clusterI{:,9},'good');
+freqGood=cellfun(@(x) str2double(regexp(x,'[0-9.]*','match','once'))>=2,table2cell(clusterI(:,8)));
+cluster_ids = table2array(clusterI(waveformGood & freqGood,1));
+save('SPK_TS.mat','TS','ids','clusterI','waveformGood','freqGood','cluster_ids');
+
+
+
+cd D:\Data\191016-DPA-Learning3_31_g0\191016-DPA-Learning3_31_g0_imec1
+tic
+addpath('D:\code\neuropixel-utils')
+addpath('D:\code\npy-matlab\npy-matlab\')
+addpath(genpath('D:\code\Kilosort2'))
+channelMapFile='D:\code\neuropixel-utils\map_files\neuropixPhase3B2_kilosortChanMap.mat';
+lwd=pwd();
+fn=ls('*.ap.bin');
+imec=Neuropixel.ImecDataset(fullfile(lwd,fn),'ChannelMap',channelMapFile);
+rmsBadChannels=imec.markBadChannelsByRMS('rmsRange',[3 100]);
+imec.writeModifiedAPMeta();
+fn=replace(fn,'.imec0.','_0.imec.');
+fn=replace(fn,'.imec1.','_1.imec.');
+cleanedPath = fullfile([lwd,'_cleaned'],fn);
+extraMeta = struct();
+extraMeta.commonAverageReferenced = true;
+fnList = {@Neuropixel.DataProcessFn.commonAverageReference};
+imec = imec.saveTranformedDataset(cleanedPath, 'transformAP', fnList, 'extraMeta', extraMeta);
+% imec.inspectAP_timeWindow([1000,1001]);
+Neuropixel.runKiloSort2(imec,'workingDir','D:\temp');
+toc
+%% phy
+
+
+
+%% risky, heavy memory load
+tic
+sync6=imec.readSync();
+% save('sync.mat','sync6');
+syncH5=fullfile([lwd,'_cleaned'],'sync.hdf5');
+h5create(syncH5,'/sync',size(sync6),'Datatype','int8')
+h5write(syncH5,'/sync',int8(sync6))
+
+toc
