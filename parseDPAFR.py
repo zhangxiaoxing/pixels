fafe6969efae283c3fbfd79da50dd346ec3c142a
ZX <wwweagle@gmail.com>
2019-10-18 15:57:58 +0800

    add depth display

--- parseDPAFR.py
+++ parseDPAFR.py
@@ -11,33 +11,33 @@ import h5py
 import math
 import matplotlib.pyplot as plt
 
-def trialAlign(trials,oneTS):
+def trialAlign(trials, oneTS):
     oneTS=oneTS[np.bitwise_and(oneTS>=trials[0,0]-30000*5, oneTS<=(trials[-1,0]+30000*10))]
     TSidx=0
-    tIdx=0;
-    tsInfo=np.ones_like(oneTS);
+    tIdx=0
+    tsId=np.ones_like(oneTS)
     while TSidx<len(oneTS) and tIdx<len(trials):
         if oneTS[TSidx]<trials[tIdx,0]+(trials[tIdx,7]+8)*30000:
             oneTS[TSidx]-=trials[tIdx,0]
-            tsInfo[TSidx]=tIdx
+            tsId[TSidx]=tIdx
             TSidx+=1
         else:
             tIdx+=1
-    return (oneTS,tsInfo)
+    return (oneTS,tsId)
         
-def baselineVector(oneTS,tsInfo):
-    tIdices=np.unique(tsInfo)
+def baselineVector(oneTS,tsId):
+    tIdices=np.unique(tsId)
     base=[]
     for tIdx in tIdices:
-        base.append(np.sum(np.bitwise_and(np.bitwise_and(tsInfo==tIdx, oneTS>=-60000), oneTS<0))/4) #500ms bin
+        base.append(np.sum(np.bitwise_and(np.bitwise_and(tsId==tIdx, oneTS>=-60000), oneTS<0))/4) #500ms bin
     if len(base)>0 and np.std(base):
         return (np.mean(base), np.std(base))
     else:
         return (0,32767)
 
-def toHist(trials,oneTS,tsInfo,sample,delay,baseVector):
+def toHist(trials,oneTS,tsId,sample,delay,baseVector):
     sel=np.nonzero(np.bitwise_and(trials[:,4]==sample , trials[:,7]==delay))[0]
-    return ((np.histogram(oneTS[np.isin(tsInfo,sel)],np.linspace(-60000,30000*(delay+6),num=(delay+8)*2+1))[0])/len(sel)-baseVector[0])/baseVector[1]
+    return ((np.histogram(oneTS[np.isin(tsId,sel)],np.linspace(-60000,30000*(delay+6),num=(delay+8)*2+1))[0])/len(sel)-baseVector[0])/baseVector[1]
 
            
 
@@ -47,22 +47,34 @@ def alignHeatmap(spkTS,spkCluster,unitInfo,trials):
     heat46=[]
     heat83=[]
     heat86=[]
+    depth=[]
     
     spkNThresh=spkTS[-1]/s1s*2    
     
-    for idx in range(unitInfo.shape[0]):
-        wf=unitInfo.iloc[idx,8]=='good' or (math.isnan(unitInfo.iloc[idx,8]) and unitInfo.iloc[idx,3]=='good')
-        spkCount=unitInfo.iloc[idx,9]
+    for infoIdx in range(unitInfo.shape[0]):
+        spkIdx=unitInfo['id'][infoIdx]
+        wf=unitInfo.iloc[infoIdx,8]=='good' or (math.isnan(unitInfo.iloc[infoIdx,8]) and unitInfo.iloc[infoIdx,3]=='good')
+        spkCount=unitInfo.iloc[infoIdx,9]
         if spkCount>spkNThresh and wf:
-            oneTSAll=(spkTS[spkCluster==idx]).astype('int64')
-            (oneTS,tsInfo)=trialAlign(trials,oneTSAll)
-            baseVec=baselineVector(oneTS,tsInfo)
+            oneTSAll=(spkTS[spkCluster==spkIdx]).astype('int64')
+            (oneTS,tsId)=trialAlign(trials,oneTSAll)
+            baseVec=baselineVector(oneTS,tsId)
 #            sel=np.nonzero(np.bitwise_and(trials[:,4]==4 , trials[:,7]==3))[0]
-            heat43.append(toHist(trials,oneTS,tsInfo,4,3,baseVec))
-            heat46.append(toHist(trials,oneTS,tsInfo,4,6,baseVec))
-            heat83.append(toHist(trials,oneTS,tsInfo,8,3,baseVec))
-            heat86.append(toHist(trials,oneTS,tsInfo,8,6,baseVec))
-    return (heat43,heat46,heat83,heat86)
+            heat43.append(toHist(trials,oneTS,tsId,4,3,baseVec))
+            heat46.append(toHist(trials,oneTS,tsId,4,6,baseVec))
+            heat83.append(toHist(trials,oneTS,tsId,8,3,baseVec))
+            heat86.append(toHist(trials,oneTS,tsId,8,6,baseVec))
+            depth.append(unitInfo['depth'][infoIdx])
+            
+    depth=np.array(depth)
+    dIdx=np.argsort(depth)
+    
+    heat43=np.array(heat43)
+    heat46=np.array(heat46)
+    heat83=np.array(heat83)
+    heat86=np.array(heat86)
+            
+    return (heat43[dIdx,:],heat46[dIdx,:],heat83[dIdx,:],heat86[dIdx,:],depth[dIdx])
 
 def plotOne(data,delay,ax,ylbl):
     
@@ -84,23 +96,32 @@ def plotOne(data,delay,ax,ylbl):
         ax.set_ylabel('Unit #')
     
 
-def plotHeatmap(heat43,heat46,heat83,heat86):
-#    plt.figure(0,figsize=[15,15])
-#    plt.imshow(np.array(heat43),cmap='jet',vmin=-3,vmax=3)
-#    plt.figure(1,figsize=[15,15])
-#    plt.imshow(np.array(heat83),cmap='jet',vmin=-3,vmax=3)         
-#    plt.figure(2,figsize=[15,15])
-#    plt.imshow(np.array(heat46),cmap='jet',vmin=-3,vmax=3)
+def plotHeatmap(heat43,heat46,heat83,heat86,depth):
+    import os
+    import re
+    cwd=os.getcwd();
+    grps=re.search('19.*(?=_cleaned)',cwd)
+
+    
     fh=plt.figure(3,figsize=[7.5,10])
-    ax=plt.subplot(2,2,1)
-    plotOne(np.array(heat43),3,ax,True);
-    ax=plt.subplot(2,2,2)
-    plotOne(np.array(heat83),3,ax,False);
-    ax=plt.subplot(2,2,3)
-    plotOne(np.array(heat46),6,ax,True);
-    ax=plt.subplot(2,2,4)
-    plotOne(np.array(heat86),6,ax,False);
+    ax=plt.subplot(2,3,1)
+    plotOne(heat43,3,ax,True)
+    ax=plt.subplot(2,3,2)
+    plotOne(heat83,3,ax,False)
+    ax=plt.subplot(2,3,4)
+    plotOne(heat46,6,ax,True)
+    ax=plt.subplot(2,3,5)
+    plotOne(heat86,6,ax,False)
+    ax=plt.subplot(1,3,3)
+    plt.plot(3840-depth)
+    ax.set_ylabel('depth (um)')
+    ax.set_xlabel('unit #')
+    plt.minorticks_on();
+    plt.grid(b=True,which='both')
+    fh.suptitle(grps.group().replace('_cleaned',''))
+    plt.tight_layout(rect=[0,0,1,0.95])
     plt.show();
+    
     fh.savefig('heatmap.png',dpi=300,bbox_inches='tight')
     return (fh,ax)
 
@@ -117,5 +138,5 @@ if __name__=="__main__":
     with h5py.File('events.hdf5','r') as fe:
         dset=fe['trials']
         trials=np.array(dset,dtype='int64')    
-    (heat43,heat46,heat83,heat86)=alignHeatmap(spkTS,spkCluster,unitInfo,trials)
-    (fh,ax)=plotHeatmap(heat43,heat46,heat83,heat86)
+    (heat43,heat46,heat83,heat86,depth)=alignHeatmap(spkTS,spkCluster,unitInfo,trials)
+    (fh,ax)=plotHeatmap(heat43,heat46,heat83,heat86,depth)
