04d15e87aa8e046242579b24b6b22a49545e3c01
zx <wwweagle@gmail.com>
2019-10-20 08:06:41 +0800

    selectivity plot

--- parseDPAFR.py
+++ parseDPAFR.py
@@ -35,7 +35,7 @@ def baselineVector(oneTS,tsId):
     else:
         return (0,32767)
 
-def toHist(trials,oneTS,tsId,sample,delay,baseVector):
+def toHist(trials,oneTS,tsId,sample,delay,baseVector=(0,1)):
     sel=np.nonzero(np.bitwise_and(trials[:,4]==sample , trials[:,7]==delay))[0]
     return ((np.histogram(oneTS[np.isin(tsId,sel)],np.linspace(-60000,30000*(delay+6),num=(delay+8)*2+1))[0])/len(sel)-baseVector[0])/baseVector[1]
 
@@ -43,6 +43,11 @@ def toHist(trials,oneTS,tsId,sample,delay,baseVector):
 
 
 def alignHeatmap(spkTS,spkCluster,unitInfo,trials):
+    heat43Raw=[]
+    heat46Raw=[]
+    heat83Raw=[]
+    heat86Raw=[]
+    
     heat43=[]
     heat46=[]
     heat83=[]
@@ -60,6 +65,12 @@ def alignHeatmap(spkTS,spkCluster,unitInfo,trials):
             (oneTS,tsId)=trialAlign(trials,oneTSAll)
             baseVec=baselineVector(oneTS,tsId)
 #            sel=np.nonzero(np.bitwise_and(trials[:,4]==4 , trials[:,7]==3))[0]
+            heat43Raw.append(toHist(trials,oneTS,tsId,4,3))
+            heat46Raw.append(toHist(trials,oneTS,tsId,4,6))
+            heat83Raw.append(toHist(trials,oneTS,tsId,8,3))
+            heat86Raw.append(toHist(trials,oneTS,tsId,8,6))
+
+
             heat43.append(toHist(trials,oneTS,tsId,4,3,baseVec))
             heat46.append(toHist(trials,oneTS,tsId,4,6,baseVec))
             heat83.append(toHist(trials,oneTS,tsId,8,3,baseVec))
@@ -74,7 +85,13 @@ def alignHeatmap(spkTS,spkCluster,unitInfo,trials):
     heat83=np.array(heat83)
     heat86=np.array(heat86)
             
-    return (heat43[dIdx,:],heat46[dIdx,:],heat83[dIdx,:],heat86[dIdx,:],depth[dIdx])
+    heat43Raw=np.array(heat43Raw)
+    heat46Raw=np.array(heat46Raw)
+    heat83Raw=np.array(heat83Raw)
+    heat86Raw=np.array(heat86Raw)
+
+
+    return ((heat43Raw[dIdx,:],heat46Raw[dIdx,:],heat83Raw[dIdx,:],heat86Raw[dIdx,:]),(heat43[dIdx,:],heat46[dIdx,:],heat83[dIdx,:],heat86[dIdx,:]),depth[dIdx])
 
 def plotOne(data,delay,ax,ylbl):
     
@@ -85,7 +102,7 @@ def plotOne(data,delay,ax,ylbl):
         [plt.plot([x,x],ax.get_ylim(),'-w') for x in [3.5,5.5,17.5,19.5]]
         ax.set_xticks([3.5,13.5,23.5])
         ax.set_xticklabels([0,5,10])
-        ax.set_xlabel('Time (s)')
+#        ax.set_xlabel('Time (s)')
         
     elif delay==3:
         [plt.plot([x,x],ax.get_ylim(),'-w') for x in [3.5,5.5,11.5,13.5]]
@@ -94,9 +111,29 @@ def plotOne(data,delay,ax,ylbl):
     
     if ylbl:
         ax.set_ylabel('Unit #')
-    
 
-def plotHeatmap(heat43,heat46,heat83,heat86,depth):
+
+def plotOneSel(A,B,delay,ax,ylbl):
+    
+    plt.imshow((B-A)/(B+A),cmap='jet',aspect='auto',vmin=-1,vmax=1)
+
+    if delay==6:
+        [plt.plot([x,x],ax.get_ylim(),'-w') for x in [3.5,5.5,17.5,19.5]]
+        ax.set_xticks([3.5,13.5,23.5])
+        ax.set_xticklabels([0,5,10])
+        
+        
+    elif delay==3:
+        [plt.plot([x,x],ax.get_ylim(),'-w') for x in [3.5,5.5,11.5,13.5]]
+        ax.set_xticks([3.5,13.5])
+        ax.set_xticklabels([0,5])
+    
+    if ylbl:
+        ax.set_ylabel('Unit #')    
+
+    ax.set_xlabel('Time (s)')
+
+def plotHeatmap(raw,normed,depth):
     import os
     import re
     cwd=os.getcwd();
@@ -104,24 +141,35 @@ def plotHeatmap(heat43,heat46,heat83,heat86,depth):
 
     
     fh=plt.figure(3,figsize=[7.5,10])
-    ax=plt.subplot(2,3,1)
-    plotOne(heat43,3,ax,True)
+    ax=plt.subplot(3,3,1)
+    plotOne(normed[0],3,ax,True)
     ax.set_title('S1 3s delay')
-    ax=plt.subplot(2,3,2)
-    plotOne(heat83,3,ax,False)
+    ax=plt.subplot(3,3,2)
+    plotOne(normed[2],3,ax,False)
     ax.set_title('S2 3s delay')
-    ax=plt.subplot(2,3,4)
-    plotOne(heat46,6,ax,True)
+    ax=plt.subplot(3,3,4)
+    plotOne(normed[1],6,ax,True)
     ax.set_title('S1 6s delay')
-    ax=plt.subplot(2,3,5)
-    plotOne(heat86,6,ax,False)
+    ax=plt.subplot(3,3,5)
+    plotOne(normed[3],6,ax,False)
     ax.set_title('S2 6s delay')
+    #depth plot
     ax=plt.subplot(1,3,3)
     plt.plot(3840-depth)
     ax.set_ylabel('depth (um)')
     ax.set_xlabel('unit #')
     plt.minorticks_on();
     plt.grid(b=True,which='both')
+    
+    #selectivity
+    ax=plt.subplot(3,3,7)
+    plotOneSel(raw[0],raw[2],3,ax,True)
+    ax.set_title('3s selectivity')    
+
+    ax=plt.subplot(3,3,8)
+    plotOneSel(raw[1],raw[3],6,ax,False)
+    ax.set_title('6s selectivity')        
+    
     fh.suptitle(grps.group().replace('_cleaned',''))
     plt.tight_layout(rect=[0,0,1,0.95])
     plt.show();
@@ -142,5 +190,5 @@ if __name__=="__main__":
     with h5py.File('events.hdf5','r') as fe:
         dset=fe['trials']
         trials=np.array(dset,dtype='int64')    
-    (heat43,heat46,heat83,heat86,depth)=alignHeatmap(spkTS,spkCluster,unitInfo,trials)
-    (fh,ax)=plotHeatmap(heat43,heat46,heat83,heat86,depth)
+    (raw,normed,depth)=alignHeatmap(spkTS,spkCluster,unitInfo,trials)
+    (fh,ax)=plotHeatmap(raw,normed,depth)
