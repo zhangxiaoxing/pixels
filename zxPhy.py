5a904e1a22516fda94ecf5e397d5f6ebb9cf23c7
ZX <wwweagle@gmail.com>
2019-10-23 13:55:52 +0800

    phy main function

--- zxPhy.py
+++ zxPhy.py
@@ -13,5 +13,5 @@ def phyProcess(params_path, **kwargs):
     controller._save_cluster_info();
     controller.model.close()
 
-#if __name__=="__main__":
-#    phyProcess("K:\\neupix\\191015-DPA-Learning2_29_g0_imec0_cleaned\\params.py")
+if __name__=="__main__":
+    phyProcess("params.py")
