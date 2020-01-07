9437ed25a6acfd63970a9621a780e6a08ddcf1ef
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-01-06 20:47:16 -0800

    beautification

--- pyKilosort.py
+++ pyKilosort.py
@@ -8,39 +8,47 @@ Created on Wed Oct 23 14:45:30 2019
 import shlex
 import subprocess
 import os
-#import time
+
+# import time
+
 
 def run(command):
     try:
-        result=subprocess.check_output(shlex.split(command),stderr=subprocess.STDOUT)
-        return 0,result
+        result = subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT)
+        return 0, result
     except subprocess.CalledProcessError as e:
         return e.returncode, e.output
 
-def runInDir(path,cleaned=False):
+
+def runInDir(path, cleaned=False):
     os.chdir(path)
-#    status=1
-#    count=0
-#    while (status!=0):
-#        count+=1
-#        print(count)
+    #    status=1
+    #    count=0
+    #    while (status!=0):
+    #        count+=1
+    #        print(count)
     if cleaned:
-        status, out=run('matlab -noFigureWindows -batch "lwd=pwd();cleaned=true;run D:\code\zxSort.m"')
+        status, out = run(
+            'matlab -noFigureWindows -batch "lwd=pwd();cleaned=true;run D:\code\zxSort.m"'
+        )
     else:
-        status, out=run('matlab -noFigureWindows -batch "lwd=pwd();cleaned=false;run D:\code\zxSort.m"')
+        status, out = run(
+            'matlab -noFigureWindows -batch "lwd=pwd();cleaned=false;run D:\code\zxSort.m"'
+        )
     print(out)
-    if status==0:
-#            time.sleep(60)
-        cwd=os.getcwd()
+    if status == 0:
+        #            time.sleep(60)
+        cwd = os.getcwd()
         if not cleaned:
-            os.chdir(cwd+'_cleaned')
+            os.chdir(cwd + "_cleaned")
         import sys
-        sys.path.insert(1,'D:/code/')
+
+        sys.path.insert(1, "D:/code/")
         import sync
         import zxPhy
         import parseDPAFR
 
-        trials=sync.runsync()
+        trials = sync.runsync()
         zxPhy.runPhy()
         parseDPAFR.runParse()
         return (out,trials)
@@ -50,10 +58,9 @@ def runInDir(path,cleaned=False):
     
 
 
-
 #
 #
-#if __name__=="__main__":
+# if __name__=="__main__":
 #    status, out=run('matlab -noFigureWindows -batch "lwd=pwd();run D:\code\zxSort.m"')
 #    if status==0:
 #        cwd=os.getcwd()
--- sync.py
+++ sync.py
@@ -6,41 +6,43 @@ This is a temporary script file.
 """
 
 
-#import glob
+# import glob
 import h5py
 import numpy as np
-#import os
 
-
+# import os
 
 
 def parseGNGEvents(events):
-    s1s=30000
-    trials=[]
-    lastTS=-300000
-    lastCue=-1
-    cueTS=-1
-    rsps=-1
-    cue=-1
-    
-    for eidx in range(len(events)):
-        cue=events[eidx][1] & 0x0c
-        cueTS=events[eidx][0]
-        if cue>0 and cueTS>(lastTS+s1s):
-            if lastCue>0:
-                trials.append([lastTS,lastCue,rsps])
-                rsps=-1;
-            lastCue=cue;
-            lastTS=cueTS;
+    s1s = 30000
+    trials = []
+    lastTS = -300000
+    lastCue = -1
+    cueTS = -1
+    rsps = -1
+    cue = -1
 
-        if (lastCue>0 and rsps<0 
-            and events[eidx][0]>= lastTS+30000 
-            and events[eidx][0]< lastTS+90000 
-            and (events[eidx][1] & 0x03)>0):
-            rsps=events[eidx][1] & 0x03
-    
+    for eidx in range(len(events)):
+        cue = events[eidx][1] & 0x0C
+        cueTS = events[eidx][0]
+        if cue > 0 and cueTS > (lastTS + s1s):
+            if lastCue > 0:
+                trials.append([lastTS, lastCue, rsps])
+                rsps = -1
+            lastCue = cue
+            lastTS = cueTS
+
+        if (
+            lastCue > 0
+            and rsps < 0
+            and events[eidx][0] >= lastTS + 30000
+            and events[eidx][0] < lastTS + 90000
+            and (events[eidx][1] & 0x03) > 0
+        ):
+            rsps = events[eidx][1] & 0x03
+
     return trials
-        
+
 
 def parseDPAEvents(events):
     """
@@ -59,137 +61,133 @@ def parseDPAEvents(events):
         reinit sample test response
         this cue is sample
 """
-    s1s=30000
-    trials=[]
-    lastTS=-300000*20
-    lastCue=-1
-    cueTS=-1
-    rsps=-1
-    cue=-1
-    sample=-1
-    test=-1
-    sampleTS=-1
-    testTS=-1
-    
+    s1s = 30000
+    trials = []
+    lastTS = -300000 * 20
+    lastCue = -1
+    cueTS = -1
+    rsps = -1
+    cue = -1
+    sample = -1
+    test = -1
+    sampleTS = -1
+    testTS = -1
+
     for eidx in range(len(events)):
-        cue=events[eidx][1] & 0x0c
-        cueTS=events[eidx][0]
-        if cue>0 and cueTS>lastTS+s1s and cueTS<lastTS+2*s1s:
-            print("error processing evt idx ",eidx)
-        elif cue>0 and cueTS>lastTS+s1s*2 and cueTS<lastTS+s1s*8:
-            sample=lastCue
-            sampleTS=lastTS
-            test=cue
-            testTS=cueTS
-            
-            lastCue=cue
-            lastTS=cueTS
-    
-        elif cue>0 and cueTS>lastTS+s1s*8:
-            if sample > 0 and test >0:
-                trials.append([sampleTS,\
-                               testTS,\
-                               np.round(sampleTS/s1s,decimals=3),\
-                               np.round(testTS/s1s,decimals=3),\
-                               sample,\
-                               test,\
-                               rsps,\
-                               np.round((testTS-sampleTS)/s1s)-1])
-                sample=-1
-                test=-1
-                sampleTS=-1
-                testTS=-1
-                rsps=-1
-            lastCue=cue
-            lastTS=cueTS
-            
+        cue = events[eidx][1] & 0x0C
+        cueTS = events[eidx][0]
+        if cue > 0 and cueTS > lastTS + s1s and cueTS < lastTS + 2 * s1s:
+            print("error processing evt idx ", eidx)
+        elif cue > 0 and cueTS > lastTS + s1s * 2 and cueTS < lastTS + s1s * 8:
+            sample = lastCue
+            sampleTS = lastTS
+            test = cue
+            testTS = cueTS
 
-    
-    
-        if (test>0 and rsps<0 
-            and events[eidx][0]>= testTS+s1s
-            and events[eidx][0]< lastTS+2*s1s
-            and (events[eidx][1] & 0x03)>0):
-            rsps=1
-        
-    if sample > 0 and test >0:
-        trials.append([sampleTS,\
-                       testTS,\
-                       np.round(sampleTS/s1s,decimals=3),\
-                       np.round(testTS/s1s,decimals=3),\
-                       sample,\
-                       test,\
-                       rsps,\
-                       np.round((testTS-sampleTS)/s1s)-1])
-        
-        
+            lastCue = cue
+            lastTS = cueTS
+
+        elif cue > 0 and cueTS > lastTS + s1s * 8:
+            if sample > 0 and test > 0:
+                trials.append(
+                    [
+                        sampleTS,
+                        testTS,
+                        np.round(sampleTS / s1s, decimals=3),
+                        np.round(testTS / s1s, decimals=3),
+                        sample,
+                        test,
+                        rsps,
+                        np.round((testTS - sampleTS) / s1s) - 1,
+                    ]
+                )
+                sample = -1
+                test = -1
+                sampleTS = -1
+                testTS = -1
+                rsps = -1
+            lastCue = cue
+            lastTS = cueTS
+
+        if (
+            test > 0
+            and rsps < 0
+            and events[eidx][0] >= testTS + s1s
+            and events[eidx][0] < lastTS + 2 * s1s
+            and (events[eidx][1] & 0x03) > 0
+        ):
+            rsps = 1
+
+    if sample > 0 and test > 0:
+        trials.append(
+            [
+                sampleTS,
+                testTS,
+                np.round(sampleTS / s1s, decimals=3),
+                np.round(testTS / s1s, decimals=3),
+                sample,
+                test,
+                rsps,
+                np.round((testTS - sampleTS) / s1s) - 1,
+            ]
+        )
+
     return trials
-
-
-
 
 
 def getEvents():
-    syncs=np.empty([0])
-    with h5py.File('sync.hdf5','r') as fs:
-        dset=fs['sync']
-        syncs=np.array(dset,dtype='int8')    
-        syncs=syncs[0]
-    
-    
-    blockCount=0
-    ts=0
-    events=[]
-    pct=0
-    while ts<(len(syncs)):
-        currPct=ts*100//len(syncs)
-        if currPct>pct:
+    syncs = np.empty([0])
+    with h5py.File("sync.hdf5", "r") as fs:
+        dset = fs["sync"]
+        syncs = np.array(dset, dtype="int8")
+        syncs = syncs[0]
+
+    blockCount = 0
+    ts = 0
+    events = []
+    pct = 0
+    while ts < (len(syncs)):
+        currPct = ts * 100 // len(syncs)
+        if currPct > pct:
             print(currPct)
-            pct=currPct
-        
-        if syncs[ts]==0:
-            blockCount+=1
-            ts+=1
+            pct = currPct
+
+        if syncs[ts] == 0:
+            blockCount += 1
+            ts += 1
         else:
-            if blockCount<34:
-                ts+=1
+            if blockCount < 34:
+                ts += 1
             else:
-                blockCount=0
-                state=0;
-                state+=(1 if np.sum(syncs[ts+5:ts+10])>128 else 0)
-                state+=(2 if np.sum(syncs[ts+10:ts+14])>127 else 0)
-                state+=(4 if np.sum(syncs[ts+14:ts+19])>128 else 0)
-                state+=(8 if np.sum(syncs[ts+19:ts+24])>128 else 0)
-                if (not events) or events[-1][1]!=state:
-                    events.append([ts,state])
-                ts+=24
+                blockCount = 0
+                state = 0
+                state += 1 if np.sum(syncs[ts + 5 : ts + 10]) > 128 else 0
+                state += 2 if np.sum(syncs[ts + 10 : ts + 14]) > 127 else 0
+                state += 4 if np.sum(syncs[ts + 14 : ts + 19]) > 128 else 0
+                state += 8 if np.sum(syncs[ts + 19 : ts + 24]) > 128 else 0
+                if (not events) or events[-1][1] != state:
+                    events.append([ts, state])
+                ts += 24
     return events
 
-def writeEvents(events,trials):
-    with h5py.File('events.hdf5','w') as fw:
-        evtDset=fw.create_dataset('events',data=np.array(events,dtype='i4'))
-        tDset=fw.create_dataset('trials',data=np.array(trials,dtype='i4'))
-            
+
+def writeEvents(events, trials):
+    with h5py.File("events.hdf5", "w") as fw:
+        evtDset = fw.create_dataset("events", data=np.array(events, dtype="i4"))
+        tDset = fw.create_dataset("trials", data=np.array(trials, dtype="i4"))
+
 
 def runsync():
-    events=getEvents()            
-    trials=parseDPAEvents(events)
-    writeEvents(events,trials)
+    events = getEvents()
+    trials = parseDPAEvents(events)
+    writeEvents(events, trials)
     return trials
 
 
-if __name__=="__main__":
-    events=getEvents()            
-    trials=parseDPAEvents(events)
-    writeEvents(events,trials)
-    
-
-    
+if __name__ == "__main__":
+    events = getEvents()
+    trials = parseDPAEvents(events)
+    writeEvents(events, trials)
 
 
-# with open('events.csv','w',newline='\r\n') as 
-            
-            
-            
-        
-        
+# with open('events.csv','w',newline='\r\n') as
