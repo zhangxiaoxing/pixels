89fc72ef0b5d882d31dabce760bec6b47b45a05c
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-03-01 14:50:42 +0800

    plot ephys pdf for scaling

--- scale.py
+++ scale.py
@@ -8,29 +8,38 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import selectivity as zpy
 
 
-    
-path=os.getcwd()
-unitInfo = pd.read_csv(os.path.join(path, "cluster_info.tsv"), sep="\t")
+dpath = None
+if os.path.exists("/gpfsdata/home/zhangxiaoxing/pixels/DataSum/"):
+    dpath = "/gpfsdata/home/zhangxiaoxing/pixels/DataSum/"
+elif os.path.exists(r"K:\neupix\DataSum"):
+    dpath = r"K:\neupix\DataSum"
+else:
+    dpath = r"D:\neupix\DataSum"
+for path in zpy.traverse(dpath):
+    print(path)
 
-su_ids=np.load('spike_clusters.npy')
-spkTS=np.load('spike_times.npy')
+    unitInfo = pd.read_csv(os.path.join(path, "cluster_info.tsv"), sep="\t")
 
-su_ids=su_ids[(spkTS>=30000*3600) & (spkTS<30000*(3600+2400))]
-spkTS=spkTS[(spkTS>=30000*3600) & (spkTS<30000*(3600+2400))]
+    su_ids = np.load(os.path.join(path, 'spike_clusters.npy'))
+    spkTS = np.load(os.path.join(path, 'spike_times.npy'))
 
-id_set=np.unique(unitInfo['id'])
-fh=plt.figure(figsize=(4.8,9.6))
-depth_list=[]
-for one_id in id_set:
-    depthDF=unitInfo.loc[unitInfo['id']==one_id,'depth']
-    if depthDF.empty:
-        print(one_id)
-        continue
-    depth=depthDF.iat[0]
-    depth_list.append([one_id,depth])
-    xs=spkTS[su_ids==one_id]/30000
-    ys=np.ones_like(xs)*depth
-    plt.scatter(xs,ys,s=2,c='k',marker='|',edgecolors='none',alpha=0.02)
-fh.savefig(os.path.join(path, 'scale.png'), dpi=300, bbox_inches="tight")    
+    su_ids = su_ids[np.squeeze((spkTS >= 30000 * 3600) & (spkTS < 30000 * (3600 + 2400)))]
+    spkTS = spkTS[np.squeeze((spkTS >= 30000 * 3600) & (spkTS < 30000 * (3600 + 2400)))]
+
+    id_set = np.unique(unitInfo['id'])
+    fh = plt.figure(figsize=(4.8, 9.6))
+    depth_list = []
+    for one_id in id_set:
+        depthDF = unitInfo.loc[unitInfo['id'] == one_id, 'depth']
+        if depthDF.empty:
+            print(one_id)
+            continue
+        depth = depthDF.iat[0]
+        depth_list.append([one_id, depth])
+        xs = spkTS[su_ids == one_id] / 30000
+        ys = np.ones_like(xs) * depth
+        plt.scatter(xs, ys, s=8, c='k', marker='|', edgecolors='none', alpha=0.002)
+    fh.savefig(os.path.join(path, 'scale.png'), dpi=300, bbox_inches="tight")
