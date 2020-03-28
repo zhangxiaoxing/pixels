4622ce03538a5e5ea8991cc6c0142ebf3c4cd986
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-03-28 11:53:59 +0800

    more baseline stats

--- jpsth/plot_basline_trials.m
+++ jpsth/plot_basline_trials.m
@@ -1,4 +1,8 @@
-function plot_basline_trials
+function plot_basline_trials(state)
+if ~exist('state','var')
+    state=false;
+end
+
 try
     evalin('base','ft_ready');
 catch me    
@@ -6,14 +10,26 @@ catch me
 end
 su_list=importdata('..\baseline_stats.csv',',',0);
 accu=0;
-for idx=find(abs(su_list.data(:,1))>0.9)'
-    accu=accu+1;
-    disp(accu)
-    fstr=su_list.textdata{idx,1};
-    suid=su_list.textdata{idx,2};
-    plot_su(fstr,suid,su_list.textdata{idx,3});
+if state
+    for idx=find(abs(su_list.data(:,1))>0.9)'
+        accu=accu+1;
+        disp(accu)
+        fstr=su_list.textdata{idx,1};
+        suid=su_list.textdata{idx,2};
+        plot_su(fstr,suid,su_list.textdata{idx,3});
+    end
+else
+    for idx=find(abs(su_list.data(:,1))<0.2 & su_list.data(:,3)<0.001)'
+        accu=accu+1;
+        disp(accu)
+        fstr=su_list.textdata{idx,1};
+        suid=su_list.textdata{idx,2};
+        plot_su_pair(replace(fstr,'K:','D:'),suid,su_list.textdata{idx,3});
+    end
 end
 end
+
+
 
 function FT_SPIKE=plot_su(f_path,suid,currReg)
 
@@ -41,8 +57,8 @@ rsps(trl(:,7)<0 & trl(:,10)==0)=4; %miss
 [axu,axd]=plotOne(ts,11,p1,p2,rsps);
 
 [~,fp,~]=fileparts(f_path);
-sgtitle(strjoin({currReg,replace(fp,'_cleaned',''),'SU#',num2str(suid)}),'Interpreter','none')
-print(fig,strjoin({'bs_',currReg,replace(fp,'_cleaned',''),num2str(suid),'.png'},'_'),'-dpng','-painters');
+sgtitle(strjoin({currReg,replace(fp,'_cleaned',''),'SU#',suid}),'Interpreter','none')
+print(fig,strjoin({'bs_',currReg,replace(fp,'_cleaned',''),suid,'.png'},'_'),'-dpng','-painters');
 close(fig)
 toc
 end
@@ -70,7 +86,7 @@ spkId=readNPY(fullfile(rootpath,'spike_clusters.npy'));
 
 FT_SPIKE=struct();
 FT_SPIKE.label{1}=suid;
-FT_SPIKE.timestamp{1,1}=spkTS(spkId==str2num(suid));
+FT_SPIKE.timestamp{1,1}=spkTS(spkId==str2double(suid));
 %  continuous format F T struct file
 cfg=struct();
 cfg.trl=[trials(:,1)-3*sps,trials(:,1)+11*sps,zeros(size(trials,1),1)-3*sps,trials];
@@ -147,7 +163,94 @@ ylabel('baseline FR (Hz)')
 end
 
 
-function plotSegs(delayLen)
-    vertLine=[0,1,1,2]+[0,0,ones(1,2).*delayLen];
-    plot(repmat(vertLine,2,1),repmat(ylim()',1,length(vertLine)),'--k');
+function [FT_SPIKE,avail]=plot_su_pair(f_path,suid,currReg)
+
+
+tic
+
+[avail,FT_SPIKE]=pre_process(f_path,suid);
+if ~avail
+    return
+end
+
+% psth=getPsth(FT_SPIKE,num2str(suid));
+trl=FT_SPIKE.trialinfo;
+correct_sel=find(trl(:,9)==1 & trl(:,10)==1);
+err_sel=find(trl(:,10)==0);
+if nnz(correct_sel)<20 || nnz(err_sel)<20
+    return
+end
+
+
+ts{1}=arrayfun(@(x) FT_SPIKE.time{1}(FT_SPIKE.trial{1}==x),err_sel,'UniformOutput',false);
+ts{2}=arrayfun(@(x) FT_SPIKE.time{1}(FT_SPIKE.trial{1}==x),correct_sel,'UniformOutput',false);
+
+fig=figure('Color','w','Position',[100,100,600,800]);
+p1=[0.15,0.5,0.7,0.40];
+p2=[0.15,0.1,0.7,0.35];
+[axu,axd]=plotOnePair(ts,11,p1,p2);
+
+
+[~,fp,~]=fileparts(f_path);
+sgtitle(strjoin({currReg,fp,'SU#',suid}),'Interpreter','none')
+% print(fig,strjoin({currReg,fp,suid,'.png'},'_'),'-dpng','-painters');
+close(fig)
+toc
+end
+
+
+function [axu,axd]=plotOnePair(ts,rLim,p1,p2)
+%         pf=nan(0,0);
+%         bn=nan(0,0);
+axu=subplot('Position',p1);
+hold on;
+ts=ts';
+counts=cellfun(@(x) numel(x),[ts{1};ts{2}]);
+skips=ceil(max(counts)/200);
+
+count1=size(ts{1},1);
+count2=size(ts{2},1);
+
+posRange=1:size(ts{1},1);
+cellfun(@(x) plot([x{1}(1:skips:end);x{1}(1:skips:end)],repmat([x{2};x{2}+0.8],1,ceil(length(x{1})/skips)),'-r'),cellfun(@(x,y) {x,y},ts{1}(posRange),num2cell(1:length(posRange),1)','UniformOutput',false));
+
+posRange=1:size(ts{2},1);
+cellfun(@(x) plot([x{1}(1:skips:end);x{1}(1:skips:end)],repmat([x{2};x{2}+0.8]+count1+4,1,ceil(length(x{1})/skips)),'-b'),cellfun(@(x,y) {x,y},ts{2}(posRange),num2cell(1:length(posRange),1)','UniformOutput',false));
+
+xlim([-1,rLim]);
+
+set(gca,'XTick',[],'YTick',[1,count1,count1+5,count1+count2+4],'YTickLabel',[1,count1,1,count2]);
+
+ylim([0,count1+count2+5]);
+arrayfun(@(x) xline(x,'--k'),[0,1,4,7]);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+axd=subplot('Position',p2);
+binSize=0.25;
+hold on;
+
+pfHist=cell2mat(cellfun(@(x) histcounts(x,-4:binSize:rLim)./binSize,ts{1},'UniformOutput',false));
+cia=bootci(1000,@(x) mean(x), pfHist);
+fill([-4+binSize/2:binSize:rLim,rLim-binSize/2:-binSize:-4],[cia(1,:),fliplr(cia(2,:))],[1,0.8,0.8],'EdgeColor','none');
+
+
+bnHist=cell2mat(cellfun(@(x) histcounts(x,-4:binSize:rLim)./binSize,ts{2},'UniformOutput',false));
+cib=bootci(1000,@(x) mean(x), bnHist);
+fill([-4+binSize/2:binSize:rLim,rLim-binSize/2:-binSize:-4],[cib(1,:),fliplr(cib(2,:))],[0.8,0.8,1],'EdgeColor','none');
+
+plot(-4+binSize/2:binSize:rLim,(mean(pfHist,1))','-r');
+plot(-4+binSize/2:binSize:rLim,(mean(bnHist,1))','-b');
+
+xlim([-1,rLim]);
+if min(size(ts{1},1),size(ts{2},1))>2
+    ylim([min(ylim()),max([cia(:);cib(:)])]);
+end
+arrayfun(@(x) xline(x,'--k'),[0,1,4,7]);
+
+
+set(gca,'XTick',[0,5,10]);
+xlabel(sprintf('time (s), 1:%d downsample',skips))
+
+
 end
