85b5404e5df605155bc2e38444d1bf7b1e960f71
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-06-30 01:07:37 +0800

    bin transference

--- jpsth/bin_trans.m
+++ jpsth/bin_trans.m
@@ -12,6 +12,26 @@ end
 dbin=diff(pre_post_bin,1,2);
 histogram(dbin)
 
+
+pre_post_bin=[];
+for i=1:length(pref_chain_S1)
+%     pre=(pref_chain_S1(i,1:6)>0).*(1:6);  %TODO should do both pref >0 and ==1 test
+%     post=(pref_chain_S1(i,7:12)>0).*(1:6);
+      pre1st=find(pref_chain_S1(i,1:6)==1,1);
+      post1st=find(pref_chain_S1(i,7:12)==1,1);
+      if ~isempty(pre1st) && ~isempty(post1st)
+        pre_post_bin(end+1,:)=[pre1st,post1st];
+      end
+end
+% doesn't really work
+% scatter(pre_post_bin(:,1),pre_post_bin(:,2),20,'o','MarkerEdgeColor','none','MarkerFaceColor','k','MarkerFaceAlpha',0.01);
+
+dbin=diff(pre_post_bin,1,2);
+histogram(dbin)
+
+
+
+
 pre_post_bin=[];
 for i=1:length(pref_chain_S1)
      pre=(pref_chain_S1(i,1:6)>0)*(1:6)';  %TODO should do both pref >0 and ==1 test
