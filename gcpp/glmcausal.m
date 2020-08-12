3e98a2af07eed8995915be28afdc1b83bc1c300c
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-08-12 10:00:57 +0800

    fixed windows width inconsistency

--- gcpp/glmcausal.m
+++ gcpp/glmcausal.m
@@ -62,7 +62,7 @@ Xcnew = Xc(ht+1:end,:);
 [K Q] = size(Xcnew);
 p = Q*ht/w +1;
 for kk = 1:K/k
-    for ii = 1:k
+    for ii = 1:k-ht
         temp = [1];
         for jj = 1:Q
             temp = [temp (WIN*Xc(ht+(kk-1)*k+ii-1:-1:(kk-1)*k+ii,jj))']; 
@@ -74,7 +74,7 @@ end
 % Making output matrix Ysub{}
 Ynew = X(ht+1:end,:);
 for kk = 1:K/k
-    Ysub{kk} = Ynew(k*(kk-1)+1:k*kk,n);
+    Ysub{kk} = Ynew(k*(kk-1)+1:k*kk-ht,n);
 end
 
 % Logistic regression
@@ -85,10 +85,8 @@ P = length(Xsub{1,1});
 beta_old = zeros(p,1);
 % W = {Wsub{kk}} & z = {zsub{kk}}
 for kk = 1:K/k
-    eta{kk} = zeros(k,1);
-    for pp = 1:1;
-        eta{kk} = eta{kk} + Xsub{kk,pp}*beta_old(p*(pp-1)+1:p*pp);
-    end
+%     eta{kk} = zeros(k,1);
+    eta{kk} = Xsub{kk}*beta_old(1:p);
     musub{kk} = exp(eta{kk})./(1+exp(eta{kk}));
     Wsub{kk} = diag(musub{kk}).*diag(1-musub{kk});
     zsub{kk} = eta{kk} + (Ysub{kk}-musub{kk}).*(1./diag(Wsub{kk}));
@@ -106,32 +104,27 @@ devdiff = abs(devnew - devold);
 % X'Wz(beta_old) using CG
 while (i < Irmax && devdiff > Ireps)
 
-    for pp1 = 1:1;
-        for pp2 = 1:1;
-            A(p*(pp1-1)+1:p*pp1,p*(pp2-1)+1:p*pp2) = zeros(p,p);
-            for kk = 1:K/k;
-                A(p*(pp1-1)+1:p*pp1,p*(pp2-1)+1:p*pp2) = A(p*(pp1-1)+1:p*pp1,p*(pp2-1)+1:p*pp2) + Xsub{kk,pp1}'*Wsub{kk}*Xsub{kk,pp2};
-            end
-        end
+    A(1:p,1:p) = zeros(p,p);
+    for kk = 1:K/k
+        A(1:p,1:p) = A(1:p,1:p) + Xsub{kk}'*Wsub{kk}*Xsub{kk};
     end
+
     %A = A + A' - diag(diag(A));
 
-    for pp1 = 1:1;
-        b(p*(pp1-1)+1:p*pp1,1) = zeros(p,1);
-        for kk = 1:K/k;
-            b(p*(pp1-1)+1:p*pp1,1) = b(p*(pp1-1)+1:p*pp1,1) + Xsub{kk,pp1}'*Wsub{kk}*zsub{kk};
-        end
+    
+    b(1:p,1) = zeros(p,1);
+    for kk = 1:K/k
+        b(1:p,1) = b(1:p,1) + Xsub{kk}'*Wsub{kk}*zsub{kk};
     end
+    
 
     % Conjugate gradient method for symmetric postive definite matrix A
     beta_new = cgs(A,b,cgeps,cgmax,[],[],beta_old);
     beta_old = beta_new;
 
     for kk = 1:K/k
-        eta{kk} = zeros(k,1);
-        for pp = 1:1;
-            eta{kk} = eta{kk} + Xsub{kk,pp}*beta_old(p*(pp-1)+1:p*pp);
-        end
+%         eta{kk} = zeros(k,1);
+        eta{kk} = Xsub{kk}*beta_old(1:p);
         musub{kk} = exp(eta{kk})./(1+exp(eta{kk}));
         Wsub{kk} = diag(musub{kk}).*diag(1-musub{kk});
         zsub{kk} = eta{kk} + (Ysub{kk}-musub{kk}).*(1./diag(Wsub{kk}));
