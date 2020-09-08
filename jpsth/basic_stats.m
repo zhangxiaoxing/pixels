d2c2ad7aa4a550a59cbfd20570c6b7328791fbbb
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-09-08 11:54:52 +0800

    new 5-bar system

--- jpsth/basic_stats.m
+++ jpsth/basic_stats.m
@@ -1,59 +1,195 @@
 %%overall density selec
-
-conncount=[];
-for bin=1:6
-    load(sprintf('0810_selec_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
-    if bin==1
-        paircount=nnz(diff(pair_reg,1,2));
-    end
-    conncount=[conncount,nnz(diff(reg_chain_S1,1,2)),nnz(diff(reg_chain_S2,1,2))];
-    
-end
-mmm=mean(conncount)/paircount;
-stdm=std(conncount/paircount);
-% mmm/paircount;
-
-%%overall density nonsel
-
-conncount=[];
-for bin=1:4 %%FIXME: expand to all 6 bins
-    load(sprintf('0813_nonsel_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
-    if bin==1
-        paircount=nnz(diff(pair_reg,1,2));
-    end
-    conncount=[conncount,nnz(diff(reg_chain_S1,1,2)),nnz(diff(reg_chain_S2,1,2))];
-%     conncount=[conncount,length(reg_chain_S1),length(reg_chain_S2)];
-end
-mmn=mean(conncount)/paircount;
-stdn=std(conncount/paircount);
-% mmn/paircount;
-
-%%stats
-[~,~,p]=crosstab((1:244128+1526311)>244128 ,[(1:244128)>46158,(1:1526311)>202185 ])
-
-%% congruent pairs
-conncount=[];
-for bin=1:6
-    load(sprintf('0814_selec_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
+%% further classifyed memory unit connections
+% conncount=[];
+% for bin=1:6
+%     load(sprintf('0831_selec_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
 %     if bin==1
 %         paircount=nnz(diff(pair_reg,1,2));
 %     end
-    congruS1=nnz(reg_chain_S1(:,1)~=reg_chain_S1(:,2) & pref_chain_S1(:,bin)==pref_chain_S1(:,bin+6) & pref_chain_S1(:,bin)>0);
-    congruS2=nnz(reg_chain_S2(:,1)~=reg_chain_S2(:,2) & pref_chain_S2(:,bin)==pref_chain_S2(:,bin+6) & pref_chain_S2(:,bin)>0);
-    congruPair=nnz(pair_reg(:,1)~=pair_reg(:,2) & pref_pair(:,bin)==pref_pair(:,bin+6) & pref_pair(:,bin)>0);
-    conncount=[conncount,congruS1/congruPair,congruS2/congruPair];
+%     conncount=[conncount,nnz(diff(reg_chain_S1,1,2)),nnz(diff(reg_chain_S2,1,2))];
+%
+% end
+% mmm=mean(conncount)/paircount;
+% stdm=std(conncount/paircount);
+% mmm/paircount;
+
+%%overall density nonsel
+thres=20;
+if true
+    
+    conncount=nan(114,6,2);
+    pairscount=nan(114,6);
+    for bin=1:6
+        load(sprintf('0813_nonsel_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
+        pair_regs{bin}=pair_reg;
+        reg_chains_S1{bin}=reg_chain_S1;
+        reg_chains_S2{bin}=reg_chain_S2;
+        conn_chains_S1{bin}=conn_chain_S1;
+        conn_chains_S2{bin}=conn_chain_S2;
+        pair_chains{bin}=pair_chain;
+        clear pair_reg reg_chain_S1 reg_chain_S2 pair_chain
+    end
+    for bin=1:6
+        for ses=1:114
+            selS1=conn_chains_S1{bin}(:,1)>ses*100000 & conn_chains_S1{bin}(:,1)<(ses+1)*100000;
+            selS2=conn_chains_S2{bin}(:,1)>ses*100000 & conn_chains_S2{bin}(:,1)<(ses+1)*100000;
+            conncount(ses,bin,1)=nnz(diff(reg_chains_S1{bin}(selS1,:),1,2));
+            conncount(ses,bin,2)=nnz(diff(reg_chains_S2{bin}(selS2,:),1,2));
+            pairsel=pair_chains{bin}(:,1)>ses*100000 & pair_chains{bin}(:,1)<(ses+1)*100000;
+            pairscount(ses,bin)=nnz(diff(pair_regs{bin}(pairsel,:),1,2));
+        end
+    end
+    mn=[];
+    for i=1:114
+        sescountca=[];
+        for bin=1:6
+            if pairscount(i,bin)>thres
+                sescountca=[sescountca;squeeze(conncount(i,bin,:)/pairscount(i,bin))];
+            end
+        end
+        if ~isempty(sescountca)
+            mn=[mn;i,mean(sescountca)];
+        end
+    end
+    
+    mmn=mean(mn(:,2));
+    stdn=std(mn(:,2));
+end
+%%stats
+% [~,~,p]=crosstab((1:244128+1526311)>244128 ,[(1:244128)>46158,(1:1526311)>202185 ])
+
+%% congruent pairs
+if true
+for bin=1:6
+    load(sprintf('0831_selec_conn_chain_duo_6s_%d_%d.mat',bin,bin+1));
+    pair_regs{bin}=pair_reg;
+    reg_chains_S1{bin}=reg_chain_S1;
+    reg_chains_S2{bin}=reg_chain_S2;
+    conn_chains_S1{bin}=conn_chain_S1;
+    conn_chains_S2{bin}=conn_chain_S2;
+    pair_chains{bin}=pair_chain;
+    pref_chains_S1{bin}=pref_chain_S1;
+    pref_chains_S2{bin}=pref_chain_S2;
+    pref_pairs{bin}=pref_pair;
+    clear pair_reg reg_chain_S1 reg_chain_S2 pair_chain conn_chain_S1 conn_chain_S2 pref_chain_S1 pref_chain_S2 pref_pair
+end
+end
+congru_active_count=nan(114,6,2);
+incong_active_count=nan(114,6,2);
+congru_inactive_count=nan(114,6,2);
+incong_inactive_count=nan(114,6,2);
+
+congru_active_pair=nan(114,6);
+incong_active_pair=nan(114,6);
+congru_inactive_pair=nan(114,6);
+incong_inactive_pair=nan(114,6);
+
+for bin=1:6
+    for ses=1:114
+        sessel=conn_chains_S1{bin}(:,1)>ses*100000 & conn_chains_S1{bin}(:,1)<(ses+1)*100000;
+        congru_active_count(ses,bin,1)=nnz(reg_chains_S1{bin}(sessel,1)~=reg_chains_S1{bin}(sessel,2) & pref_chains_S1{bin}(sessel,bin)==pref_chains_S1{bin}(sessel,bin+6) & pref_chains_S1{bin}(sessel,bin)>0);
+        incong_active_count(ses,bin,1)=nnz(reg_chains_S1{bin}(sessel,1)~=reg_chains_S1{bin}(sessel,2) & pref_chains_S1{bin}(sessel,bin)~=pref_chains_S1{bin}(sessel,bin+6) & pref_chains_S1{bin}(sessel,bin)>0 & pref_chains_S1{bin}(sessel,bin+6)>0);
+        congru_inactive_count(ses,bin,1)=nnz(reg_chains_S1{bin}(sessel,1)~=reg_chains_S1{bin}(sessel,2) & max(pref_chains_S1{bin}(sessel,1:6),[],2)==max(pref_chains_S1{bin}(sessel,7:12),[],2) & any(pref_chains_S1{bin}(sessel,[bin,bin+6])<1,2));
+        incong_inactive_count(ses,bin,1)=nnz(reg_chains_S1{bin}(sessel,1)~=reg_chains_S1{bin}(sessel,2) & max(pref_chains_S1{bin}(sessel,1:6),[],2)~=max(pref_chains_S1{bin}(sessel,7:12),[],2) & any(pref_chains_S1{bin}(sessel,[bin,bin+6])<1,2));
+        
+        sessel=conn_chains_S2{bin}(:,1)>ses*100000 & conn_chains_S2{bin}(:,1)<(ses+1)*100000;
+        congru_active_count(ses,bin,2)=nnz(reg_chains_S2{bin}(sessel,1)~=reg_chains_S2{bin}(sessel,2) & pref_chains_S2{bin}(sessel,bin)==pref_chains_S2{bin}(sessel,bin+6) & pref_chains_S2{bin}(sessel,bin)>0);
+        incong_active_count(ses,bin,2)=nnz(reg_chains_S2{bin}(sessel,1)~=reg_chains_S2{bin}(sessel,2) & pref_chains_S2{bin}(sessel,bin)~=pref_chains_S2{bin}(sessel,bin+6) & pref_chains_S2{bin}(sessel,bin)>0 & pref_chains_S2{bin}(sessel,bin+6)>0);
+        congru_inactive_count(ses,bin,2)=nnz(reg_chains_S2{bin}(sessel,1)~=reg_chains_S2{bin}(sessel,2) & max(pref_chains_S2{bin}(sessel,1:6),[],2)==max(pref_chains_S2{bin}(sessel,7:12),[],2) & any(pref_chains_S2{bin}(sessel,[bin,bin+6])<1,2));
+        incong_inactive_count(ses,bin,2)=nnz(reg_chains_S2{bin}(sessel,1)~=reg_chains_S2{bin}(sessel,2) & max(pref_chains_S2{bin}(sessel,1:6),[],2)~=max(pref_chains_S2{bin}(sessel,7:12),[],2) & any(pref_chains_S2{bin}(sessel,[bin,bin+6])<1,2));
+        
+        sessel=pair_chains{bin}(:,1)>ses*100000 & pair_chains{bin}(:,1)<(ses+1)*100000;
+        congru_active_pair(ses,bin)=nnz(pair_regs{bin}(sessel,1)~=pair_regs{bin}(sessel,2) & pref_pairs{bin}(sessel,bin)==pref_pairs{bin}(sessel,bin+6) & pref_pairs{bin}(sessel,bin)>0);
+        incong_active_pair(ses,bin)=nnz(pair_regs{bin}(sessel,1)~=pair_regs{bin}(sessel,2) & pref_pairs{bin}(sessel,bin)~=pref_pairs{bin}(sessel,bin+6) & pref_pairs{bin}(sessel,bin)>0 & pref_pairs{bin}(sessel,bin+6)>0);
+        congru_inactive_pair(ses,bin)=nnz(pair_regs{bin}(sessel,1)~=pair_regs{bin}(sessel,2) & max(pref_pairs{bin}(sessel,1:6),[],2)==max(pref_pairs{bin}(sessel,7:12),[],2) & any(pref_pairs{bin}(sessel,[bin,bin+6])<1,2));
+        incong_inactive_pair(ses,bin)=nnz(pair_regs{bin}(sessel,1)~=pair_regs{bin}(sessel,2) & max(pref_pairs{bin}(sessel,1:6),[],2)~=max(pref_pairs{bin}(sessel,7:12),[],2) & any(pref_pairs{bin}(sessel,[bin,bin+6])<1,2));
+        
+    end
+end
+%%
+
+mca=[];
+mia=[];
+mcs=[];
+mis=[];
+for i=1:114
+    sescountca=[];
+    sescountia=[];
+    sescountcs=[];%silent
+    sescountis=[];%silent
+    for bin=1:6
+        if congru_active_pair(i,bin)>thres
+            sescountca=[sescountca;squeeze(congru_active_count(i,bin,:)/congru_active_pair(i,bin))];
+        end
+        if incong_active_pair(i,bin)>thres
+            sescountia=[sescountia;squeeze(incong_active_count(i,bin,:)/incong_active_pair(i,bin))];
+        end
+        
+        if  congru_inactive_pair(i,bin)>thres
+            sescountcs=[sescountcs;squeeze(congru_inactive_count(i,bin,:)/congru_inactive_pair(i,bin))];
+        end
+        if  incong_inactive_pair(i,bin)>thres
+            sescountis=[sescountis;squeeze(incong_inactive_count(i,bin,:)/incong_inactive_pair(i,bin))];
+        end
+    end
+    if ~isempty(sescountca)
+        mca=[mca;i,mean(sescountca)];
+    end
+    
+    if ~isempty(sescountia)
+        mia=[mia;i,mean(sescountia)];
+    end
+    
+    if ~isempty(sescountcs)
+        mcs=[mcs;i,mean(sescountcs)];
+    end
+    if ~isempty(sescountis)
+        mis=[mis;i,mean(sescountis)];
+    end
     
 end
-mmc=mean(conncount);
-stdc=std(conncount);
+
+mmca=mean(mca(:,2));
+mmia=mean(mia(:,2));
+mmcs=mean(mcs(:,2));
+mmis=mean(mis(:,2));
+
+stdca=std(mca(:,2));
+stdia=std(mia(:,2));
+stdcs=std(mcs(:,2));
+stdis=std(mis(:,2));
+
+sum(congru_active_count(congru_active_pair>thres))
+sum(congru_active_pair(congru_active_pair>thres))
+sum(congru_inactive_count(congru_inactive_pair>thres))
+sum(congru_inactive_pair(congru_inactive_pair>thres))
+sum(incong_active_count(incong_active_pair>thres))
+sum(incong_active_pair(incong_active_pair>thres))
+sum(incong_inactive_count(incong_inactive_pair>thres))
+sum(incong_inactive_pair(incong_inactive_pair>thres))
+
+
+sum(conncount(pairscount>thres))
+sum(pairscount(pairscount>thres))
+% 
+% mmc=mean(conncount);
+% stdc=std(conncount);
+% 
+% mmi=mean(incongcount);
+% stdi=std(incongcount);
+% 
+% mms=mean(silentcount);
+% stds=std(silentcount);
+
 
 close all
-figure('Color','w','Position',[100,100,230,130])
+fh=figure('Color','w','Position',[100,100,235,270]);
 hold on
-bar([mmn,mmm,mmc],'FaceColor','k')
-errorbar(1:3,[mmn,mmm,mmc],[stdn/sqrt(12),stdm/sqrt(12),stdc/sqrt(12)],'k.','CapSize',16,'Color',[0.5,0.5,0.5])
-xlim([0.5,3.5])
+bar([mmn,mmis,mmia,mmcs,mmca],'FaceColor','w')
+errorbar(1:5,[mmn,mmis,mmia,mmcs,mmca],[stdn/sqrt(114),stdis/sqrt(114),stdia/sqrt(114),stdcs/sqrt(114),stdca/sqrt(114)],'k.','CapSize',12)
+xlim([0.5,5.5])
 ylim([0,0.25]);
-set(gca,'XTick',1:3,'XTickLabel',{'non-memory','memory units','congruent units'},'XTickLabelRotation',15)
-ylabel('connection density')
+set(gca,'XTick',1:5,'XTickLabel',{'Non-memory','Incongruent inactive','Incongruent active','Congruent inactive','Congruent active'},'XTickLabelRotation',30)
+ylabel('Connection density')
 exportgraphics(gcf,'pairwise-conn-dens.pdf','ContentType','vector');
+% exportgraphics(fh,'pairwise-conn-dens.png');
