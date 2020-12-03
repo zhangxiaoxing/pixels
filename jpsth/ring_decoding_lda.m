f3bd3d9925ff04311728921586a66284b6c9ce83
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2020-12-03 16:59:25 +0800

    update ring decoding to plot error trials

--- jpsth/ring_decoding_lda.m
+++ jpsth/ring_decoding_lda.m
@@ -1,18 +1,20 @@
 % bin_trial_count=quickStats(ring_list);
 midx=1;
-rpts=25
+rpts=2
 if true
 inact=false;
 cvcorr=cell(1,9);
 shufcorr=cell(1,9);
+errcorr=cell(1,9);
 trial_thres=45;
-
+err_thres=5;
 for bin=1:6
     cvcorr{bin+3}=[];
     shufcorr{bin+3}=[];
+    errcorr{bin+3}=[];
     for rpt=1:rpts
         disp([bin, rpt]);
-        [s1,s2]=collect_data(2,inact,bin,trial_thres);
+        [s1,s2,s1err,s2err]=collect_data(midx,inact,bin,trial_thres,err_thres);
         y=[zeros(trial_thres,1);ones(trial_thres,1)];
         cv=cvpartition(trial_thres,'KFold',10);
         for kf=1:cv.NumTestSets
@@ -20,6 +22,8 @@ for bin=1:6
             s2kf=s2(training(cv,kf),:);
             varsel=var(s1kf,0,1)>0 & var(s2kf,0,1)>0;
             Xkf=[s1kf(:,varsel);s2kf(:,varsel)];
+            Xerr=[s1err(:,varsel);s2err(:,varsel)];
+            yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
             ykf=y([training(cv,kf);training(cv,kf)]);
             CVLDAModel=fitcdiscr(Xkf,ykf,'DiscrimType','linear');
             s1Tkf=s1(test(cv,kf),:);
@@ -29,8 +33,10 @@ for bin=1:6
             yshufTkf=yTkf(randperm(numel(yTkf)));
             cvresult=CVLDAModel.predict(XTkf)==yTkf;
             cvshufresult=CVLDAModel.predict(XTkf)==yshufTkf;
+            cv_err_result=CVLDAModel.predict(Xerr)==yerr;
             cvcorr{bin+3}=[cvcorr{bin+3};cvresult];
             shufcorr{bin+3}=[shufcorr{bin+3};cvshufresult];
+            errcorr{bin+3}=[errcorr{bin+3};cv_err_result];
         end
     end
 end
@@ -56,7 +62,7 @@ end
 act_cvcorr=cvcorr;
 act_cvshuf=shufcorr;
 
-if true
+if false
 inact=true;
 cvcorr=cell(1,9);
 shufcorr=cell(1,9);
@@ -65,7 +71,7 @@ for bin=[-2,1:6]
     shufcorr{bin+3}=[];
     for rpt=1:rpts
         disp([bin, rpt]);
-        [s1,s2]=collect_data(2,inact,bin,trial_thres);
+        [s1,s2]=collect_data(midx,inact,bin,trial_thres,err_thres);
         y=[zeros(trial_thres,1);ones(trial_thres,1)];
         cv=cvpartition(trial_thres,'KFold',10);
         for kf=1:cv.NumTestSets
@@ -112,13 +118,15 @@ set(gca,'XTick',-1:2:5,'XTickLabel',{'ITI','1','3','5'})
 savefig(fh,sprintf('congru_%dring_inact_decode.fig',midx+2),'compact');
 print(fh,sprintf('congru_%dring_inact_decode.png',midx+2),'-dpng','-r300');
 end
-quit(0)
+if isunix
+    quit(0) 
+end
 
-quit(0)
+
 % keyboard()
 % 
 
-function [S1_rings_fr,S2_rings_fr]=collect_data(midx,inact,bin,trials)
+function [S1_rings_fr,S2_rings_fr,S1_rings_err_fr,S2_rings_err_fr]=collect_data(midx,inact,bin,trials,err_trials)
 datapath='ring_freq';
 if exist('inact','var') && inact
     flist=dir(fullfile(datapath,sprintf('*freq_inact_%d_*.mat',midx+2)));
@@ -127,27 +135,46 @@ else
 end
 
 ring_list=cell(0,8);
+err_ring_list=cell(0,8);
 for i=1:size(flist,1)
     fn=fullfile(datapath,flist(i).name);
-    load(fn,'allrings');
-    ring_list=[ring_list;allrings];
+    load(fn,'all_rings','err_rings');
+    ring_list=[ring_list;all_rings];
+    err_ring_list=[err_ring_list;err_rings];
 end
 
 ringOccur=sum(cell2mat(ring_list(:,5:6)),2);
 totalTrial=sum(cellfun(@(x) size(x,1), ring_list(:,7:8)),2);
 
 ring_list=ring_list(ringOccur>=totalTrial,:);
+err_ring_list=err_ring_list(ringOccur>=totalTrial,:);
 
 S1_rings_fr=[];
 S2_rings_fr=[];
+S1_rings_err_fr=[];
+S2_rings_err_fr=[];
 for i=1:size(ring_list,1)
+    if size(ring_list{i,7},1)<=trials ...
+            || size(ring_list{i,8},1)<=trials ...
+            || size(err_ring_list{i,7},1)<=err_trials ...
+            || size(err_ring_list{i,8},1)<=err_trials
+        continue
+    end
     s1=(ring_list{i,7}(:,bin+3));
     s2=(ring_list{i,8}(:,bin+3));
-    if nnz(~isnan(s1))>trials && nnz(~isnan(s2))>trials
+    s1_err=(err_ring_list{i,7}(:,bin+3));
+    s2_err=(err_ring_list{i,8}(:,bin+3));
+    
+    if nnz(~isnan(s1))>trials && nnz(~isnan(s2))>trials...
+        && nnz(~isnan(s1_err))>err_trials && nnz(~isnan(s2_err))>err_trials
         s1samp=randsample(s1(~isnan(s1)),trials);
         s2samp=randsample(s2(~isnan(s2)),trials);
+        s1samp_err=randsample(s1_err(~isnan(s1_err)),err_trials);
+        s2samp_err=randsample(s2_err(~isnan(s2_err)),err_trials);
         S1_rings_fr=[S1_rings_fr,s1samp];
         S2_rings_fr=[S2_rings_fr,s2samp];
+        S1_rings_err_fr=[S1_rings_err_fr,s1samp_err];
+        S2_rings_err_fr=[S2_rings_err_fr,s2samp_err];
     end
 end
 end
