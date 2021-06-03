3b671925fb0a5dd64173521cff45d1705f63820f
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2021-06-03 13:30:19 +0800

    after merge plot function

---
+++ jpsth/+bz/+hist/plot_one_stp_curve.m
@@ -0,0 +1,66 @@
+function plot_one_stp_curve(wt,ln,title_,opt)
+arguments
+    wt (1,1) struct {mustBeNonempty}
+    ln (1,1) struct {mustBeNonempty}
+    title_ (1,:) char
+    opt.plot_learning (1,1) logical = false
+end
+hdl=[];
+lgd=cell(0);
+
+% fh=figure('Color','w');
+hold on
+if isfield(wt,'congru')
+    if opt.plot_learning
+        shadow(fliplr(ln.congru(:,2:end)),200:200:2000,'r');
+        hlnc=plot(200:200:2000,fliplr(mean(ln.congru(:,2:end)))*100,'--r');
+        hdl=cat(2,hdl,hlnc);
+        lgd=cat(2,lgd,'Congruent learning');
+    end
+    shadow(fliplr(wt.congru(:,2:end)),200:200:2000,'r');
+    hwtc=plot(200:200:2000,fliplr(mean(wt.congru(:,2:end)))*100,'-r');
+    hdl=cat(2,hdl,hwtc);
+    lgd=cat(2,lgd,'Congruent welltrained');
+end
+if isfield(wt,'nonmem')
+    if opt.plot_learning
+        shadow(fliplr(ln.nonmem(:,2:end)),200:200:2000,'k');
+        hlnn=plot(200:200:2000,fliplr(mean(ln.nonmem(:,2:end)))*100,'--k');
+        hdl=cat(2,hdl,hlnn);
+        lgd=cat(2,lgd,'Nonmem learning');
+    end
+    shadow(fliplr(wt.nonmem(:,2:end)),200:200:2000,'k');
+    hwtn=plot(200:200:2000,fliplr(mean(wt.nonmem(:,2:end)))*100,'-k');
+    hdl=cat(2,hdl,hwtn);
+    lgd=cat(2,lgd,'Nonmem welltrained');
+end
+if isfield(wt,'incong')
+    if opt.plot_learning
+    shadow(fliplr(ln.incong(:,2:end)),200:200:2000,'b');
+    hlni=plot(200:200:2000,fliplr(mean(ln.incong(:,2:end)))*100,'--b');
+    hdl=cat(2,hdl,hlni);
+    lgd=cat(2,lgd,'Incongru learning');
+    end
+    shadow(fliplr(wt.incong(:,2:end)),200:200:2000,'b');
+    
+    hwti=plot(200:200:2000,fliplr(mean(wt.incong(:,2:end)))*100,'-b');
+    hdl=cat(2,hdl,hwti);
+    lgd=cat(2,lgd,'Incongru welltrained');
+end
+
+if ~isempty(hdl),legend(hdl,lgd);end
+
+title(title_);
+ylabel('Post spike increase (%)')
+ylim([-1,5])
+xlim([0,2000])
+set(gca,'XTick',500:500:2000)
+grid on
+end
+
+
+function shadow(data,xx,color)
+boots=bootstrp(1000,@(x) mean(x),data);
+ci=[prctile(boots,2.5);prctile(boots,97.5)].*100;
+fill([xx,fliplr(xx)],[ci(1,:),fliplr(ci(2,:))],color,'EdgeColor','none','FaceAlpha',0.1);
+end
--- jpsth/+bz/+hist/plot_stp_curve.m
+++ jpsth/+bz/+hist/plot_stp_curve.m
@@ -1,4 +1,4 @@
-function plot_ln_wt_stp_curve(learn_stats,wt_stats,opt)
+function plot_stp_curve(learn_stats,wt_stats,opt)
 % [learn_stats,~]=bz.hist.util.get_stp_stats(6000,'BZLN','suffix','Learning','type','neupix','criteria','Learning','any',true);
 % [wt_stats,~]=bz.hist.util.get_stp_stats(6000,'BZWT','suffix','','type','neupix','criteria','WT','any',true);
 
@@ -6,6 +6,7 @@ arguments
     learn_stats (1,1) struct
     wt_stats (1,1) struct
     opt.levels (1,:) double = 3:5
+    opt.plot_learning (1,1) logical = false
 end
 figure('Color','w')
 fidx=1;
@@ -14,11 +15,11 @@ for level=opt.levels
     ln=bz.hist.learn.get_stats_by_mem_type(learn_stats,'between',level);
     wt=bz.hist.learn.get_stats_by_mem_type(wt_stats,'between',level);
     subplot(numel(opt.levels),2,fidx);fidx=fidx+1;
-    bz.hist.learn.plotOne(wt,ln,sprintf('Between regions %d',level));
+    bz.hist.plot_one_stp_curve(wt,ln,sprintf('Between regions %d',level));
     %same    
     ln=bz.hist.learn.get_stats_by_mem_type(learn_stats,'within',level);
     wt=bz.hist.learn.get_stats_by_mem_type(wt_stats,'within',level);
     subplot(numel(opt.levels),2,fidx);fidx=fidx+1;
-    bz.hist.learn.plotOne(wt,ln,sprintf('Within regions %d',level));
+    bz.hist.plot_one_stp_curve(wt,ln,sprintf('Within regions %d',level));
 end
 end
