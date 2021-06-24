031bd205048da9ff68d79635642b6f9fa670556f
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2021-06-24 14:19:42 +0800

    per session COM shift showcase. Implementation of possible spike waveform filter in functional coupling

--- jpsth/+bz/load_sig_pair.m
+++ jpsth/+bz/load_sig_pair.m
@@ -6,14 +6,16 @@ arguments
     opt.prefix (1,:) char = 'BZWT'
     opt.criteria (1,:) char {mustBeMember(opt.criteria,{'Learning','WT','any'})} = 'WT'
     opt.inhibit (1,1) logical = false
+    opt.wf_criteria (1,1) logical = false
 end
-persistent sig pair type_ criteria_ prefix_ inhibit_
+persistent sig pair type_ criteria_ prefix_ inhibit_ wf_
 if isempty(sig) ...
         || (opt.pair && isempty(pair))...
         || ~strcmp(opt.type,type_)...
         || ~strcmp(opt.criteria,criteria_)...
         || ~strcmp(opt.prefix,prefix_)...
-        || inhibit_~=opt.inhibit
+        || inhibit_~=opt.inhibit...
+        || wf_~=opt.wf_criteria
     if strcmp(opt.criteria,'Learning') && strcmp(opt.type,'MY')
         fl=dir(fullfile('mydata',sprintf('%s_conn_w_reg_my_learning_*.mat',opt.prefix)));
     elseif strcmp(opt.type,'MY')
@@ -27,37 +29,48 @@ if isempty(sig) ...
             fl=fl(~inhibitsel);
         end
     end
-
+    
     fprintf('Total sessions %d\n',size(fl,1));
     if size(fl,1)<9, warning('Files not found');return; end
     sig=struct(); % for significant connect
     sig.suid=cell(0); % cluster id assigned by kilosort, 2nd+ probe prefixed by probe#
     sig.reg=cell(0); % brain region tree
-%     sig.wrsp=cell(0); % Wilcoxon rank summation p value
-%     sig.selec=cell(0); % selectivity index
+    %     sig.wrsp=cell(0); % Wilcoxon rank summation p value
+    %     sig.selec=cell(0); % selectivity index
     sig.sess=cell(0);
     sig.mem_type=cell(0); % 0=NM,1=S1 sust, 2=S1 trans, 3=S2 sust, 4=S2 trans,-1=switched, see epys.get_mem_type
     sig.wf_good=cell(0);
-
+    
     if opt.pair, pair=sig; end% for all pairs
-
-%     fields={'suid','reg','wrsp','selec','mem_type'};
+    
+    %     fields={'suid','reg','wrsp','selec','mem_type'};
     fields={'suid','reg','mem_type','per_bin','wf_good'};
     for fidx=1:size(fl,1)
         disp(fidx);
         fstr=load(fullfile(fl(fidx).folder,fl(fidx).name));
-        for fi=fields
-            sig.(fi{1}){fidx}=fstr.sig_meta.(fi{1});
-            if opt.pair, pair.(fi{1}){fidx}=fstr.pair_meta.(fi{1}); end
+        if opt.wf_criteria
+            for fi=fields
+                sig.(fi{1}){fidx}=fstr.sig_meta.(fi{1})(all(fstr.sig_meta.wf_good,2),:,:);
+                if opt.pair, pair.(fi{1}){fidx}=fstr.pair_meta.(fi{1})(all(fstr.sig_meta.wf_good,2),:,:); end
+            end
+            sig.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem,'type',opt.type,'criteria',opt.criteria),nnz(all(fstr.sig_meta.wf_good,2)),1);
+            if opt.pair, pair.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem),nnz(all(fstr.sig_meta.wf_good,2)),1); end
+            
+        else
+            for fi=fields
+                sig.(fi{1}){fidx}=fstr.sig_meta.(fi{1});
+                if opt.pair, pair.(fi{1}){fidx}=fstr.pair_meta.(fi{1}); end
+            end
+            sig.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem,'type',opt.type,'criteria',opt.criteria),size(fstr.sig_meta.suid,1),1);
+            if opt.pair, pair.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem),size(fstr.pair_meta.suid,1),1); end
         end
-        sig.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem,'type',opt.type,'criteria',opt.criteria),size(fstr.sig_meta.suid,1),1);
-        if opt.pair, pair.sess{fidx}=repmat(ephys.path2sessid(fstr.pc_stem),size(fstr.pair_meta.suid,1),1); end
     end
-
+    
     for fi=[fields,{'sess'}]
         sig.(fi{1})=cell2mat(sig.(fi{1})');
         if opt.pair, pair.(fi{1})=cell2mat(pair.(fi{1})'); end
     end
+    
 end
 sig_=sig;
 pair_=pair;
@@ -65,4 +78,5 @@ type_=opt.type;
 prefix_=opt.prefix;
 criteria_=opt.criteria;
 inhibit_=opt.inhibit;
+wf_=opt.wf_criteria;
 end
