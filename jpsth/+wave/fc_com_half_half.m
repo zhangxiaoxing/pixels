05cd1abd9a5f39e103532c8e563be89a1430a359
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2021-08-31 18:18:36 +0800

    further class fc to fwd and rev in delta com analysis

--- jpsth/+wave/fc_com_half_half.m
+++ jpsth/+wave/fc_com_half_half.m
@@ -1,26 +1,34 @@
 [sig,pair]=bz.load_sig_pair();
 rpts=50;
-sums=cell(116,2);
-parfor sess=1:2
-    ffcc=[];
-    nonfc=[];
+sums=cell(116,3);
+parfor sess=1:116
+    [fc_fwd,fc_rev,non_fc]=deal([]);
     disp(sess)
     sess_fc=sig.suid(sig.sess==sess,:);
-    fc_set=[bitshift(sess_fc(:,1),14)+sess_fc(:,2);bitshift(sess_fc(:,2),14)+sess_fc(:,1)];
+    com_map=wave.get_com_map('onepath',['SPKINFO/',ephys.sessid2path(sess)],'curve',false,'rnd_half',false);
+    fc_set_fwd=bitshift(sess_fc(:,1),14)+sess_fc(:,2);
+    fc_set_rev=bitshift(sess_fc(:,2),14)+sess_fc(:,1);
     for samp=1:2
         [stats,key_map]=shuffle_one(sess,rpts,sprintf('s%da',samp),sprintf('s%db',samp));
         comvar=cellfun(@(x) nanstd(x),stats);
+        commm=cellfun(@(x) nanmean(x),stats);
         for key=reshape(cell2mat(key_map.keys),1,[])
             suid1=double(bitshift(key,-14));
             suid2=double(bitand(key,hex2dec('3FFF')));
-            if ismember(key,fc_set)
-                ffcc=[ffcc;sess,samp,suid1,suid2,comvar(key_map(key))];
+            if ~all(arrayfun(@(x) com_map.(sprintf('s%d',sess)).(sprintf('s%d',samp)).isKey(x),[suid1,suid2]))
+                continue;
+            end
+            fc_com=arrayfun(@(x) com_map.(sprintf('s%d',sess)).(sprintf('s%d',samp))(x),[suid1,suid2]);
+            if (ismember(key,fc_set_fwd) && diff(fc_com)>0) || (ismember(key,fc_set_rev) && diff(fc_com)<0)
+                fc_fwd=[fc_fwd;sess,samp,suid1,suid2,commm(key_map(key)),comvar(key_map(key))];
+            elseif (ismember(key,fc_set_rev) && diff(fc_com)>0) || (ismember(key,fc_set_fwd) && diff(fc_com)<0)
+                fc_rev=[fc_rev;sess,samp,suid1,suid2,commm(key_map(key)),comvar(key_map(key))];
             else
-                nonfc=[nonfc;sess,samp,suid1,suid2,comvar(key_map(key))];
+                non_fc=[non_fc;sess,samp,suid1,suid2,commm(key_map(key)),comvar(key_map(key))];
             end
         end
     end
-    sums(sess,:)={ffcc,nonfc};
+    sums(sess,:)={fc_fwd,fc_rev,non_fc};
 end
 
 function [stats,key_map]=shuffle_one(sid,rpts,field_a,field_b)
@@ -32,6 +40,9 @@ fs=sprintf('s%d',sid);
 for rpt=1:2:2*rpts
     com_map=wave.get_com_map('onepath',['SPKINFO/',ephys.sessid2path(sid)],'curve',false,'rnd_half',true);
     samp_key=num2cell(intersect(cell2mat(com_map.(fs).(field_a).keys),cell2mat(com_map.(fs).(field_b).keys)));
+    if numel(samp_key)<2
+        break;
+    end
     one_comb=nchoosek(cell2mat(samp_key),2);
     for ci=1:size(one_comb,1)
         if ~key_map.isKey(getkey(one_comb(ci,:)))
