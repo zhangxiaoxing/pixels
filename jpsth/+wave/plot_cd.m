4d0fbe82be9c23506b8cc8a45dd9e438c10ab1dc
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2022-01-10 20:03:39 +0800

    split plot of selective neuron groups

--- jpsth/+wave/plot_cd.m
+++ jpsth/+wave/plot_cd.m
@@ -4,6 +4,10 @@ arguments
     opt.stats_rpt (1,1) double {mustBePositive,mustBeInteger} =5
     opt.plot_traj (1,1) logical = true
     opt.alt_proj (1,1) logical = false
+    opt.cd_pc_proj (1,1) logical = true
+    opt.plot_1st_trial (1,1) logical = false
+    opt.gen_movie (1,1) logical = true
+    opt.mem_type (1,:) char {mustBeMember(opt.mem_type,{'mem','3s','6s','both'})} = '6s'
 end
 % persistent com_str onepath_ delay_ selidx_ decision_ rnd_half_ curve_
 
@@ -13,7 +17,7 @@ end
 [~,~,sessmap]=ephys.sessid2path(0);
 homedir=ephys.util.getHomedir('type','raw');
 
-[s1mat3,s2mat3,s1mat6,s2mat6,s1mat31,s2mat31,s1mat61,s2mat61,waveids]=deal([]);
+[s1_FR_3,s2_FR_3,s1_FR_6,s2_FR_6,s1_FR_3_t1,s2_FR_3_t1,s1_FR_6_t1,s2_FR_6_t1,waveids]=deal([]);
 [s1shufmat,s2shufmat]=deal(cell(1,2*opt.stats_rpt));
 
 for ii=reshape(cell2mat(sessmap.keys()),1,[])
@@ -27,14 +31,6 @@ for ii=reshape(cell2mat(sessmap.keys()),1,[])
 
     wavesess=ones(size(suid))*double(ii);
     waveidsess=ephys.get_wave_id(wavesess,suid,'early',false,'ctx',true);
-%     s1sel3=waveids==1;
-%     s1sel6=waveids==3;
-%     s1selb=waveids==5;
-%     
-%     s2sel3=waveids==2;
-%     s2sel6=waveids==4;
-%     s2selb=waveids==6;
-
 
     s1d3=trials(:,5)==4 & trials(:,8)==3 & trials(:,9)>0 & trials(:,10)>0 & dur_resp(:,3)>1;
     s2d3=trials(:,5)==8 & trials(:,8)==3 & trials(:,9)>0 & trials(:,10)>0 & dur_resp(:,3)>1;
@@ -60,46 +56,63 @@ for ii=reshape(cell2mat(sessmap.keys()),1,[])
             s2shufmat{rpt*2}=[s2shufmat{rpt*2};shiftdim(mean(fr(s2b,msel,1:44)),1)];
         end
     end
-    wavesel=ismember(waveidsess,1:6);
+    switch opt.mem_type
+        case 'mem'
+            wavesel=ismember(waveidsess,1:6);
+        case '3s'
+            wavesel=ismember(waveidsess,1:2);
+        case '6s'
+            wavesel=ismember(waveidsess,3:4);
+        case 'both'
+            wavesel=ismember(waveidsess,5:6);
+    end
+
     if ~any(wavesel)
         continue;
     end
 
-    s1mat3=[s1mat3;shiftdim(mean(fr(s1d3,wavesel,1:44),1),1)];
-    s2mat3=[s2mat3;shiftdim(mean(fr(s2d3,wavesel,1:44),1),1)];
-    s1mat6=[s1mat6;shiftdim(mean(fr(s1d6,wavesel,1:44),1),1)];
-    s2mat6=[s2mat6;shiftdim(mean(fr(s2d6,wavesel,1:44),1),1)];
+    s1_FR_3=[s1_FR_3;shiftdim(mean(fr(s1d3,wavesel,1:44),1),1)];
+    s2_FR_3=[s2_FR_3;shiftdim(mean(fr(s2d3,wavesel,1:44),1),1)];
+    s1_FR_6=[s1_FR_6;shiftdim(mean(fr(s1d6,wavesel,1:44),1),1)];
+    s2_FR_6=[s2_FR_6;shiftdim(mean(fr(s2d6,wavesel,1:44),1),1)];
 
-    s1mat31=[s1mat31;shiftdim(mean(fr(s1d31,wavesel,1:44),1),1)];
-    s2mat31=[s2mat31;shiftdim(mean(fr(s2d31,wavesel,1:44),1),1)];
-    s1mat61=[s1mat61;shiftdim(mean(fr(s1d61,wavesel,1:44),1),1)];
-    s2mat61=[s2mat61;shiftdim(mean(fr(s2d61,wavesel,1:44),1),1)];
-
-
-    if ~(isequal(size(s1mat3),size(s1mat31)) ...
-            && isequal(size(s2mat3),size(s2mat31))...
-            && isequal(size(s1mat6),size(s1mat61))...
-            && isequal(size(s2mat6),size(s2mat61)))
-        keyboard()
-    end
+    s1_FR_3_t1=[s1_FR_3_t1;shiftdim(mean(fr(s1d31,wavesel,1:44),1),1)];
+    s2_FR_3_t1=[s2_FR_3_t1;shiftdim(mean(fr(s2d31,wavesel,1:44),1),1)];
+    s1_FR_6_t1=[s1_FR_6_t1;shiftdim(mean(fr(s1d61,wavesel,1:44),1),1)];
+    s2_FR_6_t1=[s2_FR_6_t1;shiftdim(mean(fr(s2d61,wavesel,1:44),1),1)];
 
     waveids=[waveids;waveidsess(wavesel)];
 
 end
 
-normfr=normalize([s1mat3,s2mat3,s1mat6,s2mat6,s1mat31,s2mat31,s1mat61,s2mat61].','range').';
-[s1mat3,s2mat3,s1mat6,s2mat6]=deal(normfr(:,1:44),normfr(:,(1:44)+44),normfr(:,(1:44)+88),normfr(:,(1:44)+132));
-[s1mat31,s2mat31,s1mat61,s2mat61]=deal(normfr(:,(1:44)+44*4),normfr(:,(1:44)+44*5),normfr(:,(1:44)+44*6),normfr(:,(1:44)+44*7));
+normfr=normalize([s1_FR_3,s2_FR_3,s1_FR_6,s2_FR_6,s1_FR_3_t1,s2_FR_3_t1,s1_FR_6_t1,s2_FR_6_t1].','range').';
+[s1_FR_3,s2_FR_3,s1_FR_6,s2_FR_6]=deal(normfr(:,1:44),normfr(:,(1:44)+44),normfr(:,(1:44)+88),normfr(:,(1:44)+132));
+[s1_FR_3_t1,s2_FR_3_t1,s1_FR_6_t1,s2_FR_6_t1]=deal(normfr(:,(1:44)+44*4),normfr(:,(1:44)+44*5),normfr(:,(1:44)+44*6),normfr(:,(1:44)+44*7));
+s1_3only=waveids==1;
+s2_3only=waveids==2;
+
+s1_6only=waveids==3;
+s2_6only=waveids==4;
+
+s1_both=waveids==5;
+s2_both=waveids==6;
+
+% save('FR_data.mat','s1_FR_3','s2_FR_3','s1_FR_6','s2_FR_6','s1_FR_3','s2_FR_3','s1_FR_6','s2_FR_6','s1_both',"s1_3only","s1_6only",'s2_both',"s2_3only","s2_6only")
+
+[coef,score,latent]=pca(normfr.');
+
+[s1pc3,s2pc3,s1pc6,s2pc6]=deal(score(1:44,:),score((1:44)+44,:),score((1:44)+88,:),score((1:44)+132,:));
+
 if opt.alt_proj
     %3s vs 6s axis
 else
-    cdMat=[s2mat3,s2mat6]-[s1mat3,s1mat6];
+    cdMat=[s2_FR_3,s2_FR_6]-[s1_FR_3,s1_FR_6];
     cdDelay=mean(cdMat(:,[17:28,(17:40)+44]),2);
     cdDelay=cdDelay/norm(cdDelay);
-    [cdDelay3,cdDelay6,cdDelayB]=deal(cdDelay);
-    cdDelay3(~ismember(waveids,1:2))=0;
-    cdDelay6(~ismember(waveids,3:4))=0;
-    cdDelayB(~ismember(waveids,5:6))=0;
+%     [cdDelay3,cdDelay6,cdDelayB]=deal(cdDelay);
+%     cdDelay3(~ismember(waveids,1:2))=0;
+%     cdDelay6(~ismember(waveids,3:4))=0;
+%     cdDelayB(~ismember(waveids,5:6))=0;
     
 %     cdDelayM=mean(cdMat(:,21:24),2);
 %     cdDelayM=cdDelayM/norm(cdDelayM);
@@ -125,37 +138,58 @@ if opt.plot_traj
     % plot3(proj1E(1:5),proj1M(1:5),proj1L(1:5),':r.','LineWidth',1)
     % plot3(proj2E(1:5),proj2M(1:5),proj2L(1:5),':b.','LineWidth',1)
 
-    [proj1E3,proj1M3,proj1L3]=deal(smooth(s1mat3.'*cdDelay3),smooth(s1mat3.'*cdDelay6),smooth(s1mat3.'*cdDelayB));
-    [proj2E3,proj2M3,proj2L3]=deal(smooth(s2mat3.'*cdDelay3),smooth(s2mat3.'*cdDelay6),smooth(s2mat3.'*cdDelayB));
-
-    [proj1E6,proj1M6,proj1L6]=deal(smooth(s1mat6.'*cdDelay3),smooth(s1mat6.'*cdDelay6),smooth(s1mat6.'*cdDelayB));
-    [proj2E6,proj2M6,proj2L6]=deal(smooth(s2mat6.'*cdDelay3),smooth(s2mat6.'*cdDelay6),smooth(s2mat6.'*cdDelayB));
-
-    [proj1E31,proj1M31,proj1L31]=deal(smooth(s1mat31.'*cdDelay3),smooth(s1mat31.'*cdDelay6),smooth(s1mat31.'*cdDelayB));
-    [proj2E31,proj2M31,proj2L31]=deal(smooth(s2mat31.'*cdDelay3),smooth(s2mat31.'*cdDelay6),smooth(s2mat31.'*cdDelayB));
-    [proj1E61,proj1M61,proj1L61]=deal(smooth(s1mat61.'*cdDelay3),smooth(s1mat61.'*cdDelay6),smooth(s1mat61.'*cdDelayB));
-    [proj2E61,proj2M61,proj2L61]=deal(smooth(s2mat61.'*cdDelay3),smooth(s2mat61.'*cdDelay6),smooth(s2mat61.'*cdDelayB));
-
-
-
-
-    v=VideoWriter('cd_devp_movie.mp4','MPEG-4');
-    open(v);
+    
+    if opt.cd_pc_proj
+        [proj1X3,proj1Y3,proj1Z3]=deal((s1pc3(:,1)),(s1pc3(:,2)),(s1_FR_3.'*cdDelay));
+        [proj2X3,proj2Y3,proj2Z3]=deal((s2pc3(:,1)),(s2pc3(:,2)),(s2_FR_3.'*cdDelay));
+        [proj1X6,proj1Y6,proj1Z6]=deal((s1pc6(:,1)),(s2pc6(:,2)),(s1_FR_6.'*cdDelay));
+        [proj2X6,proj2Y6,proj2Z6]=deal((s2pc6(:,1)),(s2pc6(:,2)),(s2_FR_6.'*cdDelay));
+    else
+        [proj1X3,proj1Y3,proj1Z3]=deal(smooth(s1_FR_3.'*cdDelay3),smooth(s1_FR_3.'*cdDelay6),smooth(s1_FR_3.'*cdDelayB));
+        [proj2X3,proj2Y3,proj2Z3]=deal(smooth(s2_FR_3.'*cdDelay3),smooth(s2_FR_3.'*cdDelay6),smooth(s2_FR_3.'*cdDelayB));
+        [proj1X6,proj1Y6,proj1Z6]=deal(smooth(s1_FR_6.'*cdDelay3),smooth(s1_FR_6.'*cdDelay6),smooth(s1_FR_6.'*cdDelayB));
+        [proj2X6,proj2Y6,proj2Z6]=deal(smooth(s2_FR_6.'*cdDelay3),smooth(s2_FR_6.'*cdDelay6),smooth(s2_FR_6.'*cdDelayB));
+    end
+    if opt.plot_1st_trial
+        [proj1E31,proj1M31,proj1L31]=deal(smooth(s1_FR_3_t1.'*cdDelay3),smooth(s1_FR_3_t1.'*cdDelay6),smooth(s1_FR_3_t1.'*cdDelayB));
+        [proj2E31,proj2M31,proj2L31]=deal(smooth(s2_FR_3_t1.'*cdDelay3),smooth(s2_FR_3_t1.'*cdDelay6),smooth(s2_FR_3_t1.'*cdDelayB));
+        [proj1E61,proj1M61,proj1L61]=deal(smooth(s1_FR_6_t1.'*cdDelay3),smooth(s1_FR_6_t1.'*cdDelay6),smooth(s1_FR_6_t1.'*cdDelayB));
+        [proj2E61,proj2M61,proj2L61]=deal(smooth(s2_FR_6_t1.'*cdDelay3),smooth(s2_FR_6_t1.'*cdDelay6),smooth(s2_FR_6_t1.'*cdDelayB));
+    end
+    if opt.gen_movie
+        v=VideoWriter(sprintf('cd_pc_devp_movie_%s.mp4',opt.mem_type),'MPEG-4');
+        open(v);
+    end
     fh=figure('Color','w','Position',[100,100,900,600]);
     hold on;
-    for tt=44%1:44
+    if strcmp(opt.mem_type,'3s')
+        span=32;
+    else
+        span=44;
+    end
+       
+    for tt=1:span
         cla
-        view([-5,10])
-        h13=plot3(proj1E3(1:tt),proj1M3(1:tt),proj1L3(1:tt),'-c','LineWidth',1.5);
-        h23=plot3(proj2E3(1:tt),proj2M3(1:tt),proj2L3(1:tt),'-','Color',[0.9290 0.6940 0.1250],'LineWidth',1.5);
-        h16=plot3(proj1E6(1:tt),proj1M6(1:tt),proj1L6(1:tt),'-k','LineWidth',1.5);
-        h26=plot3(proj2E6(1:tt),proj2M6(1:tt),proj2L6(1:tt),'-r','LineWidth',1.5);
+        switch opt.mem_type
+            case '3s'
+                view([5,-5])
+            case '6s'
+                view([6.5,10.5])
+            case 'both'
+                view([85,6.25])
+            case 'mem'
+                view([246,-5])
+        end
+        h13=plot3(proj1X3(1:tt),proj1Y3(1:tt),proj1Z3(1:tt),'-c','LineWidth',1.5);
+        h23=plot3(proj2X3(1:tt),proj2Y3(1:tt),proj2Z3(1:tt),'-','Color',[0.9290 0.6940 0.1250],'LineWidth',1.5);
+        h16=plot3(proj1X6(1:tt),proj1Y6(1:tt),proj1Z6(1:tt),'-k','LineWidth',1.5);
+        h26=plot3(proj2X6(1:tt),proj2Y6(1:tt),proj2Z6(1:tt),'-r','LineWidth',1.5);
         legend([h26,h23,h16,h13],{'S1-6s trial','S1-3s trial','S2-6s trial','S2-3s trial'},'FontSize',16,'Location','eastoutside','Orientation','vertical','AutoUpdate','off')
 
-        th1=text(proj1E3(tt),proj1M3(tt),proj1L3(tt),'S2-3s','Color','c','FontSize',16);
-        th2=text(proj2E3(tt),proj2M3(tt),proj2L3(tt),'S1-3s','Color',[0.9290 0.6940 0.1250],'FontSize',16);
-        th3=text(proj1E6(tt),proj1M6(tt),proj1L6(tt),'S2-6s','Color','k','FontSize',16);
-        th4=text(proj2E6(tt),proj2M6(tt),proj2L6(tt),'S1-6s','Color','r','FontSize',16);
+        th1=text(proj1X3(tt),proj1Y3(tt),proj1Z3(tt),'S2-3s','Color','c','FontSize',16);
+        th2=text(proj2X3(tt),proj2Y3(tt),proj2Z3(tt),'S1-3s','Color',[0.9290 0.6940 0.1250],'FontSize',16);
+        th3=text(proj1X6(tt),proj1Y6(tt),proj1Z6(tt),'S2-6s','Color','k','FontSize',16);
+        th4=text(proj2X6(tt),proj2Y6(tt),proj2Z6(tt),'S1-6s','Color','r','FontSize',16);
         pause(0.5)
         if tt<13
             tag='ITI';
@@ -170,26 +204,72 @@ if opt.plot_traj
         else
             tag='6s Test';
         end
-        title(sprintf('%s (%d msec)',tag,tt*250-4000),'FontSize',20)
-        xlabel('3s selective')
-        ylabel('6s selective')
-        zlabel('Both selective')
-        for frame=1:15
-%             writeVideo(v,getframe(fh))
+        title(sprintf('%s selective, %s (%d msec)',opt.mem_type,tag,tt*250-4000),'FontSize',20)
+
+        if opt.cd_pc_proj
+            xlabel('PC1')
+            ylabel('PC2')
+            zlabel('CD')
+%             xlim([-7,18])
+%             ylim([-6,8])
+%             zlim([-4,6])
+        else
+            xlabel('3s selective')
+            ylabel('6s selective')
+            zlabel('Both selective')
+        end
+        if opt.gen_movie
+            for frame=1:15
+                writeVideo(v,getframe(fh))
+            end
         end
     end
-    close(v);
-    keyboard
+    if opt.gen_movie
+        close(v);
+    end
+    savefig(sprintf('cd_pc_traj_%s',opt.mem_type));
+
+    % speed vs distance
+    for tt=2:span
+        dist3(tt-1)=norm([proj1X3(tt),proj1Y3(tt),proj1Z3(tt)]-[proj2X3(tt),proj2Y3(tt),proj2Z3(tt)]);
+        dist6(tt-1)=norm([proj1X6(tt),proj1Y6(tt),proj1Z6(tt)]-[proj2X6(tt),proj2Y6(tt),proj2Z6(tt)]);
+        speed31(tt-1)=norm([proj1X3(tt),proj1Y3(tt),proj1Z3(tt)]-[proj1X3(tt-1),proj1Y3(tt-1),proj1Z3(tt-1)]);
+%         speed32
+        speed61(tt-1)=norm([proj1X6(tt),proj1Y6(tt),proj1Z6(tt)]-[proj1X6(tt-1),proj1Y6(tt-1),proj1Z6(tt-1)]);
+%         speed62
+    end
+
+    %% dist vs speed
+    fsh=figure('Color','w','Position',[32,32,800,400]);
+    hold on
+    d3h=plot(dist3,'--r','LineWidth',1);
+    d6h=plot(dist6,'-r','LineWidth',1);
+    s3h=plot(speed31,'--b','LineWidth',1);
+    s6h=plot(speed61,'-b','LineWidth',1);
+    set(gca,'XTick',3.5:12:span,'XTickLabel',-3:3:((span-20)/4))
+    arrayfun(@(x) xline(x,':k'),[11.5,15.5,27.5,31.5,39.5,43.5])
+    xlim([1,span]);
+    xlabel('Time (s)')
+    ylabel('A.U.')
+    legend([d3h,d6h,s3h,s6h],{'S1/S2 distance, 3s Delay','S1/S2 distance, 6s Delay','S1 trajectory forefront speed, 3s Delay','S1 trajectory forefront speed, 6s Delay'},'Location','eastoutside')
+    exportgraphics(fsh,sprintf('Dist_speed_%s.pdf',opt.mem_type),'ContentType','vector')
+    %%
+    keyboard();
+
+%     h13=plot3(proj1X3(1:tt),proj1Y3(1:tt),proj1Z3(1:tt),'-c','LineWidth',1.5);
+%     h23=plot3(proj2X3(1:tt),proj2Y3(1:tt),proj2Z3(1:tt),'-','Color',[0.9290 0.6940 0.1250],'LineWidth',1.5);
+%     h16=plot3(proj1X6(1:tt),proj1Y6(1:tt),proj1Z6(1:tt),'-k','LineWidth',1.5);
+%     h26=plot3(proj2X6(1:tt),proj2Y6(1:tt),proj2Z6(1:tt),'-r','LineWidth',1.5);
 
     delete(th1)
     delete(th2)
     delete(th3)
     delete(th4)
 
-    th1=text(proj1E3(20),proj1M3(20),proj1L3(20),'S2-3s','Color','c','FontSize',16);
-    th2=text(proj2E3(24),proj2M3(24),proj2L3(24),'S1-3s','Color',[0.9290 0.6940 0.1250],'FontSize',16);
-    th3=text(proj1E6(28),proj1M6(28),proj1L6(28),'S2-6s','Color','k','FontSize',16);
-    th4=text(proj2E6(32),proj2M6(32),proj2L6(32),'S1-6s','Color','r','FontSize',16);
+    th1=text(proj1X3(20),proj1Y3(20),proj1Z3(20),'S2-3s','Color','c','FontSize',16);
+    th2=text(proj2X3(24),proj2Y3(24),proj2Z3(24),'S1-3s','Color',[0.9290 0.6940 0.1250],'FontSize',16);
+    th3=text(proj1X6(28),proj1Y6(28),proj1Z6(28),'S2-6s','Color','k','FontSize',16);
+    th4=text(proj2X6(32),proj2Y6(32),proj2Z6(32),'S1-6s','Color','r','FontSize',16);
 
     v=VideoWriter('cd_rotate_movie.mp4','MPEG-4');
     open(v);
@@ -201,31 +281,28 @@ if opt.plot_traj
     end
     close(v)
 
-
-    h131=plot3(proj1E31(1:tt),proj1M31(1:tt),proj1L31(1:tt),'--c','LineWidth',1.5);
-    h231=plot3(proj2E31(1:tt),proj2M31(1:tt),proj2L31(1:tt),'--','Color',[0.9290 0.6940 0.1250],'LineWidth',1.5);
-    h161=plot3(proj1E61(1:tt),proj1M61(1:tt),proj1L61(1:tt),'--k','LineWidth',1.5);
-    h261=plot3(proj2E61(1:tt),proj2M61(1:tt),proj2L61(1:tt),'--r','LineWidth',1.5);
-
-    v=VideoWriter('cd_rotate_movie_blockN1.mp4','MPEG-4');
+    if opt.plot_1st_trial
+        h131=plot3(proj1E31(1:tt),proj1M31(1:tt),proj1L31(1:tt),'--c','LineWidth',1.5);
+        h231=plot3(proj2E31(1:tt),proj2M31(1:tt),proj2L31(1:tt),'--','Color',[0.9290 0.6940 0.1250],'LineWidth',1.5);
+        h161=plot3(proj1E61(1:tt),proj1M61(1:tt),proj1L61(1:tt),'--k','LineWidth',1.5);
+        h261=plot3(proj2E61(1:tt),proj2M61(1:tt),proj2L61(1:tt),'--r','LineWidth',1.5);
+    end
+    v=VideoWriter('cd_PC_rotate_movie_blockN1.mp4','MPEG-4');
     open(v);
-    for dg=-5:10:355
-        view([dg,10])
+    for dg=120:10:480
+        view([dg,30])
         for fm=1:10
             writeVideo(v,getframe(fh))
         end
     end
     close(v)
 
-
-
-
-    distmat=[proj1E6-proj1E3,proj1M6-proj1M3,proj1L6-proj1L3];
+    distmat=[proj1X6-proj1X3,proj1Y6-proj1Y3,proj1Z6-proj1Z3];
     distmat1=[proj1E61-proj1E31,proj1M61-proj1M31,proj1L61-proj1L31];
     dist_3_6=arrayfun(@(x) norm(distmat(x,:)),1:44);
     dist_31_61=arrayfun(@(x) norm(distmat1(x,:)),1:44);
 
-    distmat2=[proj2E6-proj2E3,proj2M6-proj2M3,proj2L6-proj2L3];
+    distmat2=[proj2X6-proj2X3,proj2Y6-proj2Y3,proj2Z6-proj2Z3];
     distmat21=[proj2E61-proj2E31,proj2M61-proj2M31,proj2L61-proj2L31];
     dist_3_6_2=arrayfun(@(x) norm(distmat2(x,:)),1:44);
     dist_31_61_2=arrayfun(@(x) norm(distmat21(x,:)),1:44);
@@ -244,12 +321,12 @@ if opt.plot_traj
     exportgraphics(f36,'cd_dist_3s_6s.pdf','ContentType','vector')
 
 
-    distmat=[proj1E6-proj2E6,proj1M6-proj2M6,proj1L6-proj2L6];
+    distmat=[proj1X6-proj2X6,proj1Y6-proj2Y6,proj1Z6-proj2Z6];
     distmat1=[proj1E61-proj2E61,proj1M61-proj2M61,proj1L61-proj2L61];
     dist_3_6=arrayfun(@(x) norm(distmat(x,:)),1:44);
     dist_31_61=arrayfun(@(x) norm(distmat1(x,:)),1:44);
 
-    distmat2=[proj2E6-proj2E3,proj2M6-proj2M3,proj2L6-proj2L3];
+    distmat2=[proj2X6-proj2X3,proj2Y6-proj2Y3,proj2Z6-proj2Z3];
     distmat21=[proj2E61-proj2E31,proj2M61-proj2M31,proj2L61-proj2L31];
     dist_3_6_2=arrayfun(@(x) norm(distmat2(x,:)),1:44);
     dist_31_61_2=arrayfun(@(x) norm(distmat21(x,:)),1:44);
