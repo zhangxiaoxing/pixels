906bff20bcacc462b179f10feb152970ded109bf
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2022-02-01 19:51:40 +0800

    updated ordinal decoding

--- jpsth/+wave/get_ordinal_mat.m
+++ jpsth/+wave/get_ordinal_mat.m
@@ -1,107 +1,122 @@
 %TODO brain region filter, olfaction filter.
 % function out=get_ordinal_mat(opt)
 %% gen data
-[~,~,sessmap]=ephys.sessid2path(0);
-meta=ephys.util.load_meta();
-homedir=ephys.util.getHomedir('type','raw');
-for ii=reshape(cell2mat(sessmap.keys()),1,[])
-    disp(ii)
-    fpath=fullfile(homedir,sessmap(ii),"FR_All_ 250.hdf5");
-    fr=h5read(fpath,'/FR_All');
-    trials=h5read(fpath,'/Trials');
-    suid=h5read(fpath,'/SU_id');
+if false
+    [~,~,sessmap]=ephys.sessid2path(0);
+    meta=ephys.util.load_meta();
+    homedir=ephys.util.getHomedir('type','raw');
+    for ii=reshape(cell2mat(sessmap.keys()),1,[])
+        disp(ii)
+        fpath=fullfile(homedir,sessmap(ii),"FR_All_ 250.hdf5");
+        fr=h5read(fpath,'/FR_All');
+        trials=h5read(fpath,'/Trials');
+        suid=h5read(fpath,'/SU_id');
 
-    dur_resp=behav.tag_block(trials,'wt',false);
-    block_meta=[trials,dur_resp(:,end)];
-    sesssel=meta.sess==ii;
-    regsel=strcmp(meta.reg_tree(2,sesssel),'CTX');
+        dur_resp=behav.tag_block(trials,'wt',false);
+        block_meta=[trials,dur_resp(:,end)];
+        sesssel=meta.sess==ii;
+        regsel=strcmp(meta.reg_tree(2,sesssel),'CTX');
 
-    fr=fr(:,regsel,:);
+        fr=fr(:,regsel,:);
 
-    out.(sprintf('s%d',ii)).fr=fr;
-    out.(sprintf('s%d',ii)).block_meta=block_meta;
-end
-ordmat=out;
-frmap=struct();
-trlCount=[];
-for skey=reshape(fieldnames(ordmat),1,[])
-    trls=ordmat.(skey{1}).block_meta;
-    csel=trls(:,9)~=0 & trls(:,10)~=0 & ismember(trls(:,8),[3 6]) & ismember(trls(:,11),1:4);
-    sess_lbl=arrayfun(@(x) sprintf('b%dt%d',trls(x,8),trls(x,11)),find(csel),'UniformOutput',false);
-    frs=mean(ordmat.(skey{1}).fr(csel,:,1:10),3); % [nTrl,nSU]
-    trlTypes=["b3t1","b3t2","b3t3","b3t4","b6t1","b6t2","b6t3","b6t4"];
-    trlSess=nan(1,8);
-    for trlIdx=1:8
-        trlType=trlTypes(trlIdx);
-        trlSess(trlIdx)=nnz(strcmp(sess_lbl,trlType));
-        if ~isfield(frmap,trlType)
-            frmap.(trlType)=containers.Map('KeyType','char','ValueType','any');
-        end
-        for suidx=1:size(frs,2) % TODO: proper batch init
-            frmap.(trlType)(sprintf('%su%d',skey{1},suidx))=frs(strcmp(sess_lbl,trlType),suidx);
-        end
+        out.(sprintf('s%d',ii)).fr=fr;
+        out.(sprintf('s%d',ii)).block_meta=block_meta;
     end
-    trlCount=[trlCount;trlSess];
+    ordmat=out;
+    frmap=struct();
+    trlCount=[];
+    for skey=reshape(fieldnames(ordmat),1,[])
+        trls=ordmat.(skey{1}).block_meta;
+        csel=trls(:,9)~=0 & trls(:,10)~=0 & ismember(trls(:,8),[3 6]) & ismember(trls(:,11),1:4);
+        sess_lbl=arrayfun(@(x) sprintf('b%dt%d',trls(x,8),trls(x,11)),find(csel),'UniformOutput',false);
+        frs=mean(ordmat.(skey{1}).fr(csel,:,1:10),3); % [nTrl,nSU]
+        trlTypes=["b3t1","b3t2","b3t3","b3t4","b6t1","b6t2","b6t3","b6t4"];
+        trlSess=nan(1,8);
+        for trlIdx=1:8
+            trlType=trlTypes(trlIdx);
+            trlSess(trlIdx)=nnz(strcmp(sess_lbl,trlType));
+            if ~isfield(frmap,trlType)
+                frmap.(trlType)=containers.Map('KeyType','char','ValueType','any');
+            end
+            for suidx=1:size(frs,2) % TODO: proper batch init
+                frmap.(trlType)(sprintf('%su%d',skey{1},suidx))=frs(strcmp(sess_lbl,trlType),suidx);
+            end
+        end
+        trlCount=[trlCount;trlSess];
+    end
+
 end
-
-
 %% available trials for least repeated condition
 %figure();histogram(trlCount(:),1:32)
 
 
 %% circle of PCA
-keys=frmap.b3t1.keys();
-pcamat=[...
-    cellfun(@(x) mean(x),frmap.b3t1.values(keys));...
-    cellfun(@(x) mean(x),frmap.b3t2.values(keys));...
-    cellfun(@(x) mean(x),frmap.b3t3.values(keys));...
-    cellfun(@(x) mean(x),frmap.b3t4.values(keys));...
-    cellfun(@(x) mean(x),frmap.b6t1.values(keys));...
-    cellfun(@(x) mean(x),frmap.b6t2.values(keys));...
-    cellfun(@(x) mean(x),frmap.b6t3.values(keys));...
-    cellfun(@(x) mean(x),frmap.b6t4.values(keys))];
+if true
+    sukeys=frmap.b3t1.keys();
+    trlSel=ismember(cellfun(@(x) str2double(regexp(x,'(?<=s)\d*(?=u)','match','once')),frmap.b3t1.keys()),find(min(trlCount,[],2)>rpt));
+    sukeys=sukeys(trlSel);
+    pcamat=[...
+        cellfun(@(x) mean(x),frmap.b3t1.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b3t2.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b3t3.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b3t4.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b6t1.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b6t2.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b6t3.values(sukeys));...
+        cellfun(@(x) mean(x),frmap.b6t4.values(sukeys))];
 
-normmat=normalize(pcamat,1,"range");
+    [normmat,C,S]=normalize(pcamat,1,"range");
 
-[coeff,score,latent]=pca(normmat);
-figure()
-for ii=1:7
-    subplot(2,4,ii)
-    plot(score(:,ii))
+    [coeff,score,latent]=pca(normmat);
+    figure()
+    for ii=1:7
+        subplot(2,4,ii)
+        plot(score(:,ii))
+    end
+
+    fh=figure('Color','w','Position',[100,100,250,225]);
+    hold on
+    plot(score([1:8,1],1),score([1:8,1],2),'-','Color',[0.5,0.5,0.5])
+
+    ph3=plot(score(1:4,1),score(1:4,2),'o','MarkerFaceColor','b','MarkerEdgeColor','none','MarkerSize',9);
+    ph6=plot(score(5:8,1),score(5:8,2),'o','MarkerFaceColor','r','MarkerEdgeColor','none','MarkerSize',9);
+
+    tidx=[1:4,1:4];
+    arrayfun(@(x) text(score(x,1),score(x,2),sprintf('No.%d',tidx(x)),'HorizontalAlignment','center','VerticalAlignment','top','FontSize',10),1:8)
+    xlabel('PC1')
+    ylabel('PC2')
+    legend([ph3,ph6],{'3s delay','6s delay'},'Location','northoutside','Orientation','horizontal')
+    xlim([-30,25])
+    exportgraphics(fh,'Block_trial_PC.pdf','ContentType','vector')
 end
-
-fh=figure('Color','w','Position',[100,100,250,225]);
-hold on
-plot(score([1:8,1],1),score([1:8,1],2),'-','Color',[0.5,0.5,0.5])
-
-ph3=plot(score(1:4,1),score(1:4,2),'o','MarkerFaceColor','b','MarkerEdgeColor','none','MarkerSize',9);
-ph6=plot(score(5:8,1),score(5:8,2),'o','MarkerFaceColor','r','MarkerEdgeColor','none','MarkerSize',9);
-
-tidx=[1:4,1:4];
-arrayfun(@(x) text(score(x,1),score(x,2),sprintf('No.%d',tidx(x)),'HorizontalAlignment','center','VerticalAlignment','top','FontSize',10),1:8)
-xlabel('PC1')
-ylabel('PC2')
-legend([ph3,ph6],{'3s delay','6s delay'},'Location','northoutside','Orientation','horizontal')
-xlim([-30,25])
-exportgraphics(fh,'Block_trial_PC.pdf','ContentType','vector')
-
 %% SVM
-% not ready for prime time
-
-% SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',false,'Cost',[0,opt.svmcost;opt.svmcost,0]);
-
 rpt=10;
 sukeys=frmap.b3t1.keys();
 trlSel=ismember(cellfun(@(x) str2double(regexp(x,'(?<=s)\d*(?=u)','match','once')),frmap.b3t1.keys()),find(min(trlCount,[],2)>rpt));
 sukeys=sukeys(trlSel);
 
-for lbl=["b3t1","b3t2","b3t3","b3t4","b6t1","b6t2","b6t3","b6t4"]
-    cell2mat(cellfun(@(x) randsample(frmap.b3t1(x),rpt),sukeys,'UniformOutput',false));
-end
 cv=cvpartition(rpt,'KFold',10);
-
-for kf=1:cv.NumTestSets
-    s1kf=s1(:,training(cv,kf),bin,rpt);
-    s2kf=s2(:,training(cv,kf),bin,rpt);
+dmat=struct();
+for lbl=["b3t1","b3t2","b3t3","b3t4","b6t1","b6t2","b6t3","b6t4"]
+    dmat.(lbl)=cell2mat(cellfun(@(x) randsample(frmap.(lbl)(x),rpt),sukeys,'UniformOutput',false));
 end
-%% mutual information
+[result,shuf]=deal([]);
+for kf=1:cv.NumTestSets
+    [xx_train,yy_train,xx_test,yy_test]=deal([]);
+    for lbl=["b3t1","b3t2","b3t3","b3t4","b6t1","b6t2","b6t3","b6t4"]
+        %         keyboard();
+        xx_train=[xx_train;dmat.(lbl)(training(cv,kf),:)];
+        xx_test=[xx_test;dmat.(lbl)(test(cv,kf),:)];
+        yy_train=[yy_train;repmat(lbl,nnz(training(cv,kf)),1)];
+        yy_test=[yy_test;repmat(lbl,nnz(test(cv,kf)),1)];
+
+    end
+    norm_train=normalize(xx_train,'center',C,'scale',S);
+    pc_train=norm_train*coeff;
+    SVMM=fitcecoc(pc_train,yy_train);
+    norm_test=normalize(xx_test,'center',C,'scale',S);
+    pc_test=norm_test*coeff;
+    modelPredict=SVMM.predict(pc_test);
+    result=[result;modelPredict==yy_test];
+    shuf=[shuf;randsample(modelPredict,numel(modelPredict))==yy_test];
+end
--- jpsth/+wave/ordinal_MI.m
+++ jpsth/+wave/ordinal_MI.m
@@ -1,11 +1,13 @@
 %TODO brain region filter, olfaction filter.
 % function out=get_ordinal_mat(opt)
 %% gen data
+if false %gendata
+    addpath('K:\Lib\MutualInfo\');
 [~,~,sessmap]=ephys.sessid2path(0);
 meta=ephys.util.load_meta();
 homedir=ephys.util.getHomedir('type','raw');
-mi_all=[];
-anovanp=[];
+[mi_all,anovanp,su_meta]=deal([]);
+
 for ii=reshape(cell2mat(sessmap.keys()),1,[])
     disp(ii)
     fpath=fullfile(homedir,sessmap(ii),"FR_All_ 250.hdf5");
@@ -28,7 +30,7 @@ for ii=reshape(cell2mat(sessmap.keys()),1,[])
             for ord=int32([31:34,61:64])
                 dur=idivide(ord,10);
                 dur_ord=rem(ord,10);
-                trl_sel=trials(:,8)==dur & dur_resp(:,3)==dur_ord & all(trials(:,9:10)>0,2);
+                trl_sel=trials(:,8)==dur & dur_resp(:,3)==dur_ord;
                 dx=mean(fr(trl_sel,suidx,win),3);
                 xx=[xx;dx];
                 yy=[yy;double(ord).*ones(size(dx))];
@@ -38,9 +40,11 @@ for ii=reshape(cell2mat(sessmap.keys()),1,[])
         end
         mi_all=[mi_all;mi];
         anovanp=[anovanp;ap];
+        su_meta=[su_meta;ii,suidx,suid(suidx)];
     end
 end
-keyboard();
+end
+if false
 sig_sel=any(anovanp(:,1:7)<0.05,2);
 mi_sel=mi_all(sig_sel,:);
 COM=mi_sel(:,1:7)*((1:7).')./sum(mi_sel(:,1:7),2);
@@ -56,5 +60,70 @@ xlabel('Time (s)')
 ylabel('Neuron #')
 arrayfun(@(x) xline(x,'--w'),[3.5,4.5]);
 exportgraphics(fh,'Ordinal_MI.pdf','ContentType','vector')
+end
+%% SU showcase
+su_sel=any(anovanp(:,1:7)<0.01 & mi_all(:,1:7)>2,2);
+su_plot=su_meta(su_sel,:);
+mi_plot=mi_all(su_sel,1:7);
+anp_plot=anovanp(su_sel,1:7);
+
+for ii=1:size(su_plot,1)
+    reg_tree=meta.reg_tree(:,meta.sess==su_plot(ii,1) & meta.allcid==su_plot(ii,3));
+    disp([anp_plot(ii,:);mi_plot(ii,:)])
+%     disp(reg_tree)
+%     if ~strcmp(reg_tree{2},'CTX')
+%         continue
+%     end
+%     [~,~,~,~,~,FT_SPIKE]=ephys.getSPKID_TS(ii,"keep_trial",true,"suids",[su_meta(ii,3)]);
+%     dur_resp=behav.tag_block(FT_SPIKE.trialinfo,'wt',false);
+    fpath=fullfile(homedir,sessmap(su_plot(ii,1)),"FR_All_ 250.hdf5");
+    fr=h5read(fpath,'/FR_All');
+    trials=h5read(fpath,'/Trials');
+    suid=h5read(fpath,'/SU_id');
+
+    dur_resp=behav.tag_block(trials,'wt',false);
+    cmap=[winter(4);autumn(4)];
+
+    fh=figure('Color','w');
+    subplot(2,1,1)
+    hold on;
+    ph=nan(8,1);
+    for ord=int32([31:34,61:64])
+        dur=idivide(ord,10);
+        dur_ord=rem(ord,10);
+        trl_sel=trials(:,8)==dur & dur_resp(:,3)==dur_ord;
+        cidx=dur_ord;
+        if dur==6
+            cidx=cidx+4;
+        end
+        ph(cidx)=plot(smooth(squeeze(mean(fr(trl_sel,su_plot(ii,2),1:28),1)),3),'Color',cmap(cidx,:));
+    end
+    arrayfun(@(x) xline(x,'--k'),[12,16]+0.5)
+    title(strjoin(reg_tree([2,5]),'-'))
+    legend(ph,{'3s No.1','3s No.2','3s No.3','3s No.4','6s No.1','6s No.2','6s No.3','6s No.4'},'location','eastoutside')
+    
+    bins=find(anp_plot(ii,:)<0.05 & mi_plot(ii,:)>2);
+    for bb=1:numel(bins)
+        subplot(numel(bins)*2,1,bb+numel(bins))
+        hold on
+        for ord=int32([31:34,61:64])
+            dur=idivide(ord,10);
+            dur_ord=rem(ord,10);
+            trl_sel=trials(:,8)==dur & dur_resp(:,3)==dur_ord & all(trials(:,9:10)>0,2);
+            cidx=dur_ord;
+            if dur==6
+                cidx=cidx+4;
+            end
+            mm=mean(fr(trl_sel,su_plot(ii,2),bins(bb)*4-3:bins(bb)*4),'all');
+            sem=std(reshape(fr(trl_sel,su_plot(ii,2),bins(bb)*4-3:bins(bb)*4),1,[]))./sqrt((nnz(trl_sel).*4));
+            bar(cidx,mm,'grouped','FaceColor',cmap(cidx,:));
+            errorbar(cidx,mm,sem,'k.')
+        end
+        title(sprintf('sess#%d|suid#%d|bin #%d',su_plot(ii,1),su_plot(ii,3),bins(bb)))
+    end
+    keyboard()
+    close(fh)
+end
+
 
 
