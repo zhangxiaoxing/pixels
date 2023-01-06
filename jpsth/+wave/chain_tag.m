6afafc943c76adbe9525a27d1f12b71c0ea8a989
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2023-01-06 17:27:49 +0800

    chains office update WIP

--- jpsth/+wave/chain_tag.m
+++ jpsth/+wave/chain_tag.m
@@ -13,12 +13,16 @@ end
 % all_chains=fieldnames(pstats.congru);
 waveids=reshape(unique(chains.wave),1,[]);
 sesses=reshape(unique(chains.sess),1,[]);
+ch_len=cellfun(@(x) numel(x),chains.cids);
 
 for sessid=sesses
-%         sess_chain=all_chains(sess==sessid);
-        [spkID,spkTS,trials,suids,folder]=ephys.getSPKID_TS(sessid);
-    for wid=waveids
-        for duration=[3 6]
+    [spkID,spkTS,trials,~,~,FT_SPIKE]=ephys.getSPKID_TS(sessid,'keep_trial',true);
+    for duration=[3 6]
+        for wid=waveids
+            if (contains(wid,'d3') && duration==6) ...
+                || (contains(wid,'d6') && duration ==3)
+                continue
+            end
             if contains(wid,'s1')
                 trial_sel=find(trials(:,5)==4 & trials(:,8)==duration & all(trials(:,9:10)>0,2));
             elseif contains(wid,'s2')
@@ -28,44 +32,61 @@ for sessid=sesses
             else
                 keyboard();
             end
+            
+            sess_indices=reshape(find(chains.sess==sessid & strcmp(chains.wave,wid) & ch_len>4),1,[]);
 
-            for rr=reshape(sess_chain,1,[])
-                if ~all(ismember(pstats.congru.(rr{1}).rstats{4},waveid),2) ...
-                        || (duration==3 && any(ismember(pstats.congru.(rr{1}).rstats{4},[2,4,8]),"all")) ...
-                        || (duration==6 && any(ismember(pstats.congru.(rr{1}).rstats{4},[1,3,7]),"all"))
-                    continue
+            for cc=sess_indices
+                ts_id=[];
+                cids=chains.cids{cc};
+%                 disp({sessid,ri});
+                for in_chain_pos=1:numel(cids) % TODO, 1:chain_len
+                    one_chain_sel=spkID==cids(in_chain_pos);
+                    rawts=spkTS(one_chain_sel);
+
+                    ft_sel=strcmp(FT_SPIKE.label,num2str(cids(in_chain_pos)));
+                    ft_ts=FT_SPIKE.timestamp{ft_sel};
+                    ft_trl_time=FT_SPIKE.time{ft_sel};
+                    ft_trl=FT_SPIKE.trial{ft_sel};
+
+                    [~,tspos]=ismember(ft_ts,rawts);
+                    ext_time=repmat(-realmax,numel(rawts),1);
+                    ext_time(tspos)=ft_trl_time;
+
+                    ext_trl=repmat(-realmax,numel(rawts),1);
+                    ext_trl(tspos)=ft_trl;
+
+                    ts_id=cat(1,ts_id,[rawts,... % 1
+                        repmat(cids(in_chain_pos),numel(rawts),1),...  % 2
+                        ones(numel(rawts),1)*in_chain_pos,...  % 3
+                        ext_time,...  % 4
+                        ext_trl]); % 5
                 end
-                ts_id=pstats.congru.(rr{1}).ts_id;
-                rcids=pstats.congru.(rr{1}).rstats{3};
-                for su=rcids
-                    sutag="s"+sessid+"w"+wid+"u"+num2str(su);
-                    if ~isfield(single_su_multi_chain.("d"+num2str(duration)),sutag)
-                        single_su_multi_chain.("d"+num2str(duration)).(sutag)=ts_id(ts_id(:,2)==su & ismember(ts_id(:,5),trial_sel),4:5);
-                        ssmr_meta.("d"+num2str(duration)).(sutag)={pstats.congru.(rr{1}).rstats};
-                    end
-                    single_su_multi_chain.("d"+num2str(duration)).(sutag)(:,end+1)=ts_id(ts_id(:,2)==su & ismember(ts_id(:,5),trial_sel) ,6);
-                    ssmr_meta.("d"+num2str(duration)).(sutag)(end+1)={pstats.congru.(rr{1}).rstats};
-                end
+                % optional remove non-wave spikes
+                ts_id=ts_id(ts_id(:,4)>=1 & ts_id(:,4)<(duration+1) & ismember(ts_id(:,5),trial_sel),:);
+                %
+                ts_id=sortrows(ts_id,1);
+                ch_tags=chain_one(ts_id(:,[1 3]),numel(chains.cids{cc}));
+                ts_id=[ts_id,ch_tags.tags]; % join TS, chain tag % 6
+
+                % TODO:output
             end
         end
     end
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 end
-function chain_one(in) 
+
+
+
+function out=chain_one(in,chain_len,opt)
+arguments
+    in
+    chain_len
+    opt.max_win (1,1) double = 10
+end
+
+
 tags=zeros(size(in,1),1);
 chain_idx=1;
 curr_pre_ptr=1;
@@ -75,57 +96,69 @@ ends=[];
 spk_cnt=[];
 durs=[];
 tsize=size(in,1);
-
+prevptr=0;
+% TODO: trial necessary?
 while curr_pre_ptr<tsize
-    if rem(curr_pre_ptr,50000)==0, fprintf('%06d.\n',curr_pre_ptr);end
+    prevptr=curr_pre_ptr;
+    if rem(curr_pre_ptr,5000)==0, fprintf('%06d.\n',curr_pre_ptr);end
     %matching time window, assuming 30kHz
     %assume max 200hz neuron FR, 2spikes × 5 su in 10ms
-
     if isempty(curr_chain)
-        cyc_number_next=rem(in(curr_pre_ptr,2)+1,rsize);
-        if cyc_number_next==0, cyc_number_next=rsize;end
-        nxtstep=min(curr_pre_ptr+20,tsize);
+        nxtstep=min(curr_pre_ptr+2*chain_len,tsize); % 200hz->2spk/su/10ms
+        nxtptr=curr_pre_ptr+find(in(curr_pre_ptr:nxtstep,2)==1,1)-1;
+        if isempty(nxtptr)
+            curr_pre_ptr=curr_pre_ptr+1;
+            continue
+        else
+            curr_pre_ptr=nxtptr;
+        end
+        cyc_number_next=2;
+        
         syn_win_ubound=find(in((curr_pre_ptr+1):nxtstep,1)>in(curr_pre_ptr,1)+300,1); %first outside window
-        if isempty(syn_win_ubound), break;end %TODO use max available instead
+        if isempty(syn_win_ubound)
+            syn_win_ubound=find(in((curr_pre_ptr+1):end,1)>in(curr_pre_ptr,1)+300,1); %first outside window
+            if isempty(syn_win_ubound)
+                break;
+            end
+        end %TODO use max available instead
         syn_win_lbound=find(in((curr_pre_ptr+1):(syn_win_ubound+curr_pre_ptr-1),1)>in(curr_pre_ptr,1)+24,1);
     else
-        cyc_number_next=rem(in(curr_chain(end),2)+1,rsize);
-        if cyc_number_next==0, cyc_number_next=rsize;end
-        nxtstep=min(curr_chain(end)+20,tsize);
-        syn_win_ubound=find(in((curr_pre_ptr+1):nxtstep,1)>in(curr_chain(end),1)+300,1); %first outside window
-        if isempty(syn_win_ubound), break;end %TODO use max available instead
-        syn_win_lbound=find(in((curr_pre_ptr+1):(syn_win_ubound+curr_pre_ptr-1),1)>in(curr_chain(end),1)+24,1);
+        cyc_number_next=in(curr_chain(end),2)+1;
+        if cyc_number_next>chain_len
+            syn_win_lbound=[];
+        else
+            nxtstep=min(curr_pre_ptr+2*chain_len,tsize);
+            syn_win_ubound=find(in((curr_pre_ptr+1):nxtstep,1)>in(curr_chain(end),1)+300,1); %first outside window
+            if isempty(syn_win_ubound)
+                syn_win_ubound=find(in((curr_pre_ptr+1):end,1)>in(curr_pre_ptr,1)+300,1); %first outside window
+                if isempty(syn_win_ubound)
+                    break;
+                end
+            end %TODO use max available instead
+            syn_win_lbound=find(in((curr_pre_ptr+1):(syn_win_ubound+curr_pre_ptr-1),1)>in(curr_chain(end),1)+24,1);
+        end
     end
     if isempty(syn_win_lbound)
         curr_chain=[];
         curr_pre_ptr=curr_pre_ptr+1;
         continue;
     end %matching time window, assuming 30kHz
-    diff_post_ptr=find(in((curr_pre_ptr+syn_win_lbound):(curr_pre_ptr+syn_win_ubound-1),2)==cyc_number_next,1); %post unit
+    diff_post_ptr=find(in((curr_pre_ptr+syn_win_lbound):(curr_pre_ptr+syn_win_ubound-1),2)==cyc_number_next,1,'last'); %post unit
     if ~isempty(diff_post_ptr)
         %TODO temp list chain spk
         if isempty(curr_chain), curr_chain=curr_pre_ptr;end
         curr_pre_ptr=curr_pre_ptr+syn_win_lbound-1+diff_post_ptr;
         curr_chain=vertcat(curr_chain,curr_pre_ptr);
     else
-        if numel(curr_chain)>rsize
+        if numel(curr_chain)==chain_len
             tags(curr_chain)=chain_idx;
             chain_idx=chain_idx+1;
-            starts(end+1)=in(curr_chain(1),2);
-            ends(end+1)=in(curr_chain(end),2);
-            spk_cnt(end+1)=numel(curr_chain);
-            durs(end+1)=in(curr_chain(end),1)-in(curr_chain(1),1);
         end
         curr_chain=[];
         curr_pre_ptr=curr_pre_ptr+1;
-        %         continue
     end
 end
 
 out=struct();
 out.tags=sparse(tags);
-out.starts=starts;
-out.ends=ends;
-out.spk_cnt=spk_cnt;
-out.durs=durs;
 end
