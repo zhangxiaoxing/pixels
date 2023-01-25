45bb1a17ccf2a7aa6413e275dff505b987572e97
ZhangXiaoxing <wwweagle@263.net>
2023-01-25 12:41:00 +0800

    variable chain burst interval

--- jpsth/+wave/chain_sust_tag.m
+++ jpsth/+wave/chain_sust_tag.m
@@ -7,6 +7,7 @@ function out=chain_sust_tag(chains,opt)
 arguments
     chains
     opt.ccg (1,1) logical = true
+    opt.burstInterval (1,1) double = 300
 end
 
 % tsidin={0,25,50,75,100,125,150};
@@ -16,7 +17,7 @@ end
 if opt.ccg
     load('sums_conn_10.mat','sums_conn_str');
 end
-%% build up
+%% set up
 % all_chains=fieldnames(pstats.congru);
 ch_len=cellfun(@(x) numel(x),chains.cids);
 waveids=reshape(unique(chains.wave(ch_len>4)),1,[]);
@@ -76,7 +77,7 @@ for sessid=sesses
                 for kk=1:numel(cids)
                     tsidin{kk}=ts_id(ts_id(:,3)==kk,1);
                 end
-                ts=chain_recur(tsidin,[],[],1);
+                ts=chain_recur(tsidin,[],[],1,'burstInterval',opt.burstInterval);
                 if ~isempty(ts)
 
                     outkey="s"+sessid+"c"+cc;
@@ -108,6 +109,7 @@ for sessid=sesses
     end
 end
 
+if false % temporary due to lack of local toolbox
 for dur=reshape(fieldnames(out),1,[])
     for wv=reshape(fieldnames(out.(dur{1})),1,[])
         for lp=reshape(fieldnames(out.(dur{1}).(wv{1})),1,[])
@@ -122,15 +124,17 @@ for dur=reshape(fieldnames(out),1,[])
         end
     end
 end
+end
 
 blame=vcs.blame();
-save('chain_sust_tag.mat','out','blame')
+blame.comment="Not all session finished, limited by available RAM";
+save(sprintf('chain_sust_tag_%d.mat',opt.burstInterval),'out','blame','-v7.3')
 stats(out);
 end
 
 function stats(out)
 perchaindur=struct();
-[perchaindur.d6.size,perchaindur.d6.dur,perchaindur.d3.size,perchaindur.d3.dur]=deal([]);
+[perchaindur.d6.size,perchaindur.d6.dur,perchaindur.d3.size,perchaindur.d3.dur,perchaindur.d6.int,perchaindur.d3.int]=deal([]);
 for dur=reshape(fieldnames(out),1,[])
 %     [perchaindur.size,perchaindur.dur]=deal([]);
     for wv=reshape(fieldnames(out.(dur{1})),1,[])
@@ -138,24 +142,32 @@ for dur=reshape(fieldnames(out),1,[])
 %             keyboard();
             perchaindur.(dur{1}).size=[perchaindur.(dur{1}).size,cellfun(@(x) size(x,1),out.(dur{1}).(wv{1}).(lp{1}).ts)];
             perchaindur.(dur{1}).dur=[perchaindur.(dur{1}).dur,cellfun(@(x) diff(x([1,end],3),1,1),out.(dur{1}).(wv{1}).(lp{1}).ts)./30];
+            perchaindur.(dur{1}).int=[perchaindur.(dur{1}).int,cell2mat(cellfun(@(x) diff(x(:,3),1,1).',out.(dur{1}).(wv{1}).(lp{1}).ts,'UniformOutput',false))./30];
         end
     end
     statss.("d"+dur)=perchaindur;
 end
 
-d6hist=histcounts(perchaindur.d6.dur,0:10:200,'Normalization','probability');
-d3hist=histcounts(perchaindur.d3.dur,0:10:200,'Normalization','probability');
+d6hist=histcounts(perchaindur.d6.dur,0:50:600,'Normalization','probability');
+d3hist=histcounts(perchaindur.d3.dur,0:50:600,'Normalization','probability');
 figure()
 hold on
-plot(5:10:195,d6hist,'-k');
-plot(5:10:195,d3hist,'--k');
+plot(25:50:575,d6hist,'-k');
+plot(25:50:575,d3hist,'--k');
 set(gca(),'XScale','log','YScale','log');
 end
 
 
-function out=chain_recur(in,chainDepth,currIdx,recDepth)
+function out=chain_recur(in,chainDepth,currIdx,recDepth,opt)
+arguments
+    in
+    chainDepth
+    currIdx
+    recDepth
+    opt.burstInterval (1,1) double = 300 % ticks, at 30k sps
+end
 % disp("recDepth"+num2str(recDepth))
-
+% disp("interval "+num2str(opt.burstInterval));
 out=cell(0); %{[depth,id]}
 clen=size(in,2);
 
@@ -166,9 +178,9 @@ end
 
 while currIdx<=numel(in{chainDepth})
     % no branch
-    if currIdx<=numel(in{chainDepth})-1 && in{chainDepth}(currIdx+1)-in{chainDepth}(currIdx)<300
+    if currIdx<=numel(in{chainDepth})-1 && in{chainDepth}(currIdx+1)-in{chainDepth}(currIdx)<opt.burstInterval
         link=[chainDepth,currIdx,in{chainDepth}(currIdx)];
-        rec=chain_recur(in,chainDepth,currIdx+1,recDepth+1);
+        rec=chain_recur(in,chainDepth,currIdx+1,recDepth+1,'burstInterval',opt.burstInterval);
 %         if chainDepth==1 && currIdx==1
 %             keyboard()
 %         end
@@ -189,14 +201,14 @@ while currIdx<=numel(in{chainDepth})
         end
         if nidx<=numel(in{nxtd}) && in{nxtd}(nidx)<in{chainDepth}(currIdx)+300 % in window
             link=[chainDepth,currIdx,in{chainDepth}(currIdx)];
-            rec=chain_recur(in,nxtd,nidx,recDepth+1);
+            rec=chain_recur(in,nxtd,nidx,recDepth+1,'burstInterval',opt.burstInterval);
             for ii=1:numel(rec)
                 out(end+1)={[link;rec{ii}]};
             end
         end
     end
-    if recDepth==1  % TODO: if branch, 13B loops, if not 11S skips
-        while currIdx<=numel(in{1})-1 && in{1}(currIdx+1)<in{1}(currIdx)+300
+    if recDepth==1
+        while currIdx<=numel(in{1})-1 && in{1}(currIdx+1)<in{1}(currIdx)+opt.burstInterval
             currIdx=currIdx+1;
         end
         currIdx=currIdx+1;
