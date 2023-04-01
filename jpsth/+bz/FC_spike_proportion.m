668e777cdfffab2fa3ef742de07e20791770649c
ZhangXiaoxing <wwweagle@263.net>
2023-04-01 14:04:59 +0800

    FC spike proportion

---
+++ jpsth/+bz/FC_spike_proportion.m
@@ -0,0 +1,62 @@
+global_init;
+wrs_mux_meta=ephys.get_wrs_mux_meta();
+
+[sig,~]=bz.load_sig_sums_conn_file('pair',false);
+sig=bz.join_fc_waveid(sig,wrs_mux_meta.wave_id);
+sig.congrusel=pct.su_pairs.get_congru(sig.waveid);
+
+usess=unique(sig.sess(sig.congrusel));
+
+sums=[];
+for sessid=reshape(usess,1,[])
+    disp(sessid)
+    usig=sig.suid(sig.sess==sessid & sig.congrusel,:);
+    wid=sig.waveid(sig.sess==sessid & sig.congrusel,:);
+    [~,~,trials,~,~,FT_SPIKE]=ephys.getSPKID_TS(sessid,'keep_trial',true);
+    FT_SPIKE.fc_tag=cell(size(FT_SPIKE.timestamp));
+    for cidx=1:numel(FT_SPIKE.timestamp)
+        FT_SPIKE.fc_tag{cidx}=zeros(size(FT_SPIKE.timestamp{cidx}),'uint8');
+    end
+
+    for ss=1:size(usig,1)
+        % TODO: pref trials
+        leadsel=strcmp(FT_SPIKE.label,num2str(usig(ss,1)));
+        folosel=strcmp(FT_SPIKE.label,num2str(usig(ss,2)));
+        [pref3,pref6]=bz.rings.preferred_trials_rings(wid(ss,:),trials);
+        for tt=reshape([pref3;pref6],1,[])
+            leadtssel=FT_SPIKE.trial{leadsel}==tt & FT_SPIKE.time{leadsel}>1 & FT_SPIKE.time{leadsel}<=(trials(tt,8)+1);
+            folotssel=FT_SPIKE.trial{folosel}==tt & FT_SPIKE.time{folosel}>1 & FT_SPIKE.time{folosel}<=(trials(tt,8)+1);
+            
+            % in preferred trial tag
+            FT_SPIKE.fc_tag{leadsel}(leadtssel)=bitor(FT_SPIKE.fc_tag{leadsel}(leadtssel),1);
+            FT_SPIKE.fc_tag{folosel}(folotssel)=bitor(FT_SPIKE.fc_tag{folosel}(folotssel),1);
+
+            if any(leadtssel) && any(folotssel)
+                leadts=FT_SPIKE.timestamp{leadsel}(leadtssel);
+                folots=FT_SPIKE.timestamp{folosel}(folotssel);
+                latency=folots-leadts.';
+                
+                leadtag=any(latency>24 & latency<=300,2);
+                folotag=any(latency>24 & latency<=300,1);
+
+                if any(leadtag)
+                    FT_SPIKE.fc_tag{leadsel}(leadtssel)=bitor(FT_SPIKE.fc_tag{leadsel}(leadtssel),uint8(leadtag*2).');
+                end
+
+                if any(folotag)
+                    FT_SPIKE.fc_tag{folosel}(folotssel)=bitor(FT_SPIKE.fc_tag{folosel}(folotssel),uint8(folotag*2));
+                end
+            end
+        end
+    end
+
+    ufccid=unique(usig);
+    for ccid=reshape(ufccid,1,[])
+        ccsel=strcmp(FT_SPIKE.label,num2str(ccid));
+        onefctag=FT_SPIKE.fc_tag{ccsel};
+        sums=[sums;sessid,ccid,nnz(bitand(onefctag,2)),nnz(bitand(onefctag,1)),numel(onefctag)];
+    end
+end
+blame=vcs.blame();
+save(fullfile("bzdata","FC_spike_proportion.mat"),'sums','blame')
+prctile(double(sums(:,3))./double(sums(:,4)),[25 50 75])
