9f2f185d941674e9befbf64cb592b3fccbd302b2
ZhangXiaoxing <wwweagle@263.net>
2023-09-21 23:02:24 +0800

    home WIP

--- jpsth/+bz/per_reg_SC_replay.m
+++ jpsth/+bz/per_reg_SC_replay.m
@@ -18,7 +18,7 @@ np_dur=[6 3 6 3 0 0];
 
 sps=30000;
 % per session
-for sess=reshape(unique(sig.sess(scids)),1,[])
+for sess=reshape(unique(sig.sess(congrusel & regsel)),1,[])
     disp(sess)
     [spkID,spkTS,~,~,~,FT_SPIKE]=ephys.getSPKID_TS(sess,'keep_trial',true,'jagged',true); 
     spkID=uint16(spkID);
@@ -33,43 +33,40 @@ for sess=reshape(unique(sig.sess(scids)),1,[])
         pref_delay_trls=all(trials(:,9:10)==1,2) & trials(:,5)==pref_samp(wids(keyidx)) & ismember(trials(:,8),setdiff([3 6],np_dur(wids(keyidx))));
         np_delay_trls=all(trials(:,9:10)==1,2) & trials(:,5)==setdiff([4,8],pref_samp(wids(keyidx))) & ismember(trials(:,8),setdiff([3 6],np_dur(wids(keyidx))));
 
-        [prefsec,prefspk,npsec,npspk,pref_iti_sec,pref_iti_spk,np_iti_sec,np_iti_spk]=deal(0);
-        for dd=[3 6]
-            %pref
-            prefsel=pref_delay_trls & trials(:,8)==dd;
-            prefsec=prefsec+nnz(prefsel).*dd;
-            coupleOne(sig.suid(scid,:),FT_SPIKE,prefsel)
-            prefspk=prefspk+nnz(ismember(FT_SPIKE.trial{ftsel},find(prefsel)) & FT_SPIKE.time{ftsel}>=1 & FT_SPIKE.time{ftsel}<4);
-            %nonpref
-            npsel=np_delay_trls & trials(:,8)==dd;
-            npsec=npsec+nnz(npsel).*dd;
-            npspk=npspk+nnz(ismember(FT_SPIKE.trial{ftsel},find(npsel)) & FT_SPIKE.time{ftsel}>=1 & FT_SPIKE.time{ftsel}<4);
-            %pref iti %
-            pref_iti_sec=pref_iti_sec+nnz(prefsel).*9;
-            pref_iti_spk=nnz(ismember(FT_SPIKE.trial{ftsel},find(prefsel)) & FT_SPIKE.time{ftsel}>=dd+5 & FT_SPIKE.time{ftsel}<dd+14); % 9s per ITI
-            if any(find(prefsel)==size(trials,1)) && (session_tick-trials(end,2))/sps<13
-                pref_iti_sec=pref_iti_sec-13+((session_tick-trials(end,2))/sps);
-            end
-            %np iti
-            np_iti_sec=np_iti_sec+nnz(npsel).*9;
-            np_iti_spk=nnz(ismember(FT_SPIKE.trial{ftsel},find(npsel)) & FT_SPIKE.time{ftsel}>=dd+5 & FT_SPIKE.time{ftsel}<dd+14); % 9s per ITI
-            if any(find(npsel)==size(trials,1)) && (session_tick-trials(end,2))/sps<13
-                np_iti_sec=np_iti_sec-13+((session_tick-trials(end,2))/sps);
-            end
+        %pref
+        prefsec=sum(trials(pref_delay_trls,8));
+        prefspk=coupleOne(sig.suid(scid,:),FT_SPIKE,pref_delay_trls,true);
+        %nonpref
+        npsec=sum(trials(np_delay_trls,8));
+        npspk=coupleOne(sig.suid(scid,:),FT_SPIKE,np_delay_trls,true);
+        %pref iti %
+        pref_iti_sec=nnz(pref_delay_trls).*9;
+        pref_iti_spk=coupleOne(sig.suid(scid,:),FT_SPIKE,pref_delay_trls,false);
+
+        if any(find(pref_delay_trls)==size(trials,1)) && (session_tick-trials(end,2))/sps<13
+            pref_iti_sec=pref_iti_sec-13+((session_tick-trials(end,2))/sps);
         end
+        %np iti
+        np_iti_sec=nnz(np_delay_trls).*9;
+        np_iti_spk=coupleOne(sig.suid(scid,:),FT_SPIKE,np_delay_trls,false);
+        if any(find(np_delay_trls)==size(trials,1)) && (session_tick-trials(end,2))/sps<13
+            np_iti_sec=np_iti_sec-13+((session_tick-trials(end,2))/sps);
+        end
+
 
         %  corresponding network in pre task, post task
         before_sec=trials(1,1)./sps;
-        before_spk=nnz(spkID==cid & spkTS<trials(1,1));
+        before_spk=coupleOuttask(sig.suid(scid,:),spkID,spkTS,0,trials(1,1));
 
         after_sec=(session_tick-trials(end,2))./sps-60;
         if after_sec<=0
             after_sec=nan;
             after_spk=nan;
         else
-            after_spk=nnz(spkID==cid & spkTS>=(trials(end,2)+60.*sps));
-        end
+            after_spk=coupleOuttask(sig.suid(scid,:),spkID,spkTS,trials(end,2)+60.*sps,session_tick);
 
+        end
+        %TODO gather result here
         cidreg=su_meta.reg_tree{5,su_meta.sess==sess & su_meta.allcid==cid};
         su_sums.(cidreg)=[su_sums.(cidreg);...
             array2table([prefspk,prefsec,npspk,npsec,pref_iti_spk,pref_iti_sec,np_iti_spk,np_iti_sec,before_spk,before_sec,after_spk,after_sec],...
@@ -100,16 +97,40 @@ end
 savefig(fh,fullfile('binary','per_reg_WM_neuron_FR_replay.fig'));
 
 
-function coupleOne(suid,FT_SPIKE,prefsel,delay)
+function spcnt=coupleOne(suid,FT_SPIKE,prefsel,delay)
 selL=strcmp(FT_SPIKE.label,num2str(suid(1)));
 selF=strcmp(FT_SPIKE.label,num2str(suid(2)));
-for t=find(prefsel)
+spcnt=0;
+for t=reshape(find(prefsel),1,[])
     if delay
-        spkL=FT_SPIKE.time{selL}(FT_SPIKE.trial{selL}==t & FT_SPIKE.time{selL}>=1... ...
-            & FT_SPIKE.time{selL}<(FT_SPIKE.trialinfo(t,8)+1)
+        spkL=FT_SPIKE.time{selL}(FT_SPIKE.trial{selL}==t & FT_SPIKE.time{selL}>=1 ...
+            & FT_SPIKE.time{selL}<(FT_SPIKE.trialinfo(t,8)+1));
+        spkF=FT_SPIKE.time{selF}(FT_SPIKE.trial{selF}==t & FT_SPIKE.time{selF}>=1 ...
+            & FT_SPIKE.time{selF}<(FT_SPIKE.trialinfo(t,8)+1));
     else %iti
-
+        spkL=FT_SPIKE.time{selL}(FT_SPIKE.trial{selL}==t & FT_SPIKE.time{selL}>=(FT_SPIKE.trialinfo(t,8)+5) ...
+            & FT_SPIKE.time{selL}<(FT_SPIKE.trialinfo(t,8)+14));
+        spkF=FT_SPIKE.time{selF}(FT_SPIKE.trial{selF}==t & FT_SPIKE.time{selF}>=(FT_SPIKE.trialinfo(t,8)+5) ...
+            & FT_SPIKE.time{selF}<(FT_SPIKE.trialinfo(t,8)+14));
     end
-
-end    
+    lagmat=(spkL-spkF.');
+    spcnt=spcnt+nnz(lagmat>0 & lagmat<0.01);
 end
+end
+
+
+function spcnt=coupleOuttask(suid,spkID,spkTS,onset,offset)
+spcnt=0;
+Lts=spkTS(spkID==suid(1) & spkTS>=onset & spkTS<offset);
+Fts=spkTS(spkID==suid(2) & spkTS>=onset & spkTS<offset);
+[~,~,Lbin]=histcounts(Lts,0:600000:offset); %20s bin
+[~,~,Fbin]=histcounts(Fts,0:600000:offset); %20s bin
+
+for b=reshape(intersect(Lbin,Fbin),1,[])
+    ltsb=Lts(Lbin==b);
+    ftsb=Fts(Fbin==b);
+    lagmat=(ltsb-ftsb.');
+    spcnt=spcnt+nnz(lagmat>0 & lagmat<300);
+end
+end
+
