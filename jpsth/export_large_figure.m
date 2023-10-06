c4ee93eca7eca2c7d3874f633b583788e145f898
ZhangXiaoxing <wwweagle@263.net>
2023-10-06 18:35:28 +0800

    house cleaning

--- jpsth/export_large_figure.m
+++
@@ -1,8 +0,0 @@
-function export_large_figure(fh,fn)
-if ~endsWith(fn,'.pdf')
-    fn=[fn,'.pdf'];
-end
-fh.PaperSize=[36,44];
-fh.PaperOrientation='portrait';
-fh.PaperPosition=[2,2,32,40];
-print(fn,'-dpdf')
